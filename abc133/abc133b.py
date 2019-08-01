e34345e8cae51f6194362aa759df278b8680bb7f
c-yan <c-yan@users.noreply.github.com>
2019-08-02 03:29:57 +0900

    不要だったアイデアの除去

--- abc133/abc133b.py
+++ abc133/abc133b.py
@@ -1,9 +1,8 @@
 l, r = [int(e) for e in input().split()]
 m = 2019
 for i in range(l, r):
-  ir = i % 2019
   for j in range(i + 1, r + 1):
-    t = (ir * j) % 2019
+    t = (i * j) % 2019
     if t < m:
       m = t
     if m == 0:
