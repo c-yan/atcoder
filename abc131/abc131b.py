4a2546843ed6a1bc4d27e82d499d2c3c5863c230
c-yan <c-yan@users.noreply.github.com>
2019-08-03 08:52:50 +0900

    abc131b

---
+++ abc131/abc131b.py
@@ -0,0 +1,9 @@
+n, l = [int(e) for e in input().split()]
+if l < 0 and n + l - 1 > 0:
+  print(sum(range(l, n + l)))
+else:
+  if abs(l) > abs(n + l - 1):
+    print(sum(range(l, n + l)) - (n + l - 1))
+  else:
+    print(sum(range(l, n + l)) - l)
+
