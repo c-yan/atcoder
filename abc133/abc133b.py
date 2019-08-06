ddd92198172231479c9b49a87c034f66a91be841
c-yan <c-yan@users.noreply.github.com>
2019-08-07 00:23:47 +0900

    abc133b

---
+++ abc133/abc133b.py
@@ -0,0 +1,10 @@
+import math
+n, d = map(int, input().split())
+x = [[int(e) for e in input().split()] for _ in range(n)]
+result = 0
+for i in range(n - 1):
+  for j in range(i + 1, n):
+    t = math.sqrt(sum((x[i][k] - x[j][k]) * (x[i][k] - x[j][k]) for k in range(d)))
+    if t == math.ceil(t):
+      result += 1
+print(result)
