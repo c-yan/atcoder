a5c457453b73ea44a56fcf74ed9c8240c93f5879
c-yan <c-yan@users.noreply.github.com>
2019-08-11 01:25:15 +0900

    abc137a, abc137b, abc137c

---
+++ abc137/abc137b.py
@@ -0,0 +1,5 @@
+k, x = map(int, input().split())
+l = max(-1000000, x - k + 1)
+r = min(1000000, x + k - 1)
+for i in range(l, r + 1):
+  print(i)
---
+++ abc137/abc137c.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = [input() for _ in range(n)]
+for i in range(n):
+  s[i] = ''.join(sorted(s[i]))
+t = {}
+for i in range(n):
+  if s[i] in t:
+    t[s[i]] += 1
+  else:
+    t[s[i]] = 1
+print(sum(t[k] * (t[k] -1) // 2 for k in t))
