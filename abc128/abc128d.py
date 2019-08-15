8d4c8f20b4652d0ca71bfeda10d4474010e5e7a2
c-yan <c-yan@users.noreply.github.com>
2019-08-15 16:27:31 +0900

    abc128d

---
+++ abc128/abc128d.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+v = [int(e) for e in input().split()]
+m = min(n, k)
+result = 0
+for i in range(m + 1):
+  for j in range(i + 1):
+    t = v[:j]
+    t.extend(v[n - (i - j):])
+    t.sort()
+    l = k - i
+    while len(t) > 0 and l > 0 and t[0] < 0:
+      t = t[1:]
+      l -= 1
+    m = sum(t)
+    if m > result:
+      result = m
+print(result)
