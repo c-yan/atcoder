fb89e61aca285fb7c5ff25a4161c4bf0b9e522a3
c-yan <c-yan@users.noreply.github.com>
2019-08-17 00:27:20 +0900

    agc026a

---
+++ agc026/agc026a.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = [int(e) for e in input().split()]
+p = a[0]
+result = 0
+for i in range(1, n):
+  if p == a[i]:
+    result += 1
+    p = -1
+  else:
+    p = a[i]
+print(result)
