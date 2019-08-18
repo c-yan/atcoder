100155da66e3619f9727e0d1b29e752c84fbd71e
c-yan <c-yan@users.noreply.github.com>
2019-08-19 04:05:50 +0900

    コンテスト後に追加されたテストが通るよう修正

--- abc138/abc138d.py
+++ abc138/abc138d.py
@@ -1,15 +1,24 @@
+from collections import deque
 def main():
-  _map, _range, _int = map, range, int
-  n, q = _map(_int, input().split())
-  values = [0] * (n + 1)
-  parents = [0] * (n + 1)
-  for _ in _range(n - 1):
-    a, b = _map(_int, input().split())
-    parents[b] = a
-  for _ in _range(q):
-    p, x = _map(_int, input().split())
-    values[p] += x
-  for i in _range(1, n + 1):
-    values[i] += values[parents[i]]
-  print(*values[1:])
+  _map, _input, _int = map, input, int
+  n, q = _map(_int, _input().split())
+  values = [0] * n
+  links = [[] for _ in range(n)]
+  for _ in range(n - 1):
+    a, b = _map(_int, _input().split())
+    links[a - 1].append(b - 1)
+    links[b - 1].append(a - 1)
+  for _ in range(q):
+    p, x = _map(_int, _input().split())
+    values[p - 1] += x
+  d = deque()
+  d.append((0, -1))
+  while len(d) != 0:
+    i, p = d.popleft()
+    for j in links[i]:
+      if j == p:
+        continue
+      values[j] += values[i]
+      d.append((j, i))
+  print(*values)
 main()
