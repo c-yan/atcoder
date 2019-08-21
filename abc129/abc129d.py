3f95c5e609f05b4ad69556bf4a21ac78fafd82b6
c-yan <c-yan@users.noreply.github.com>
2019-08-21 20:08:43 +0900

    PyPy でしか通っていなかったが、Python で通せたので入れ替え

--- abc129/abc129d.py
+++ abc129/abc129d.py
@@ -1,23 +1,42 @@
-h, w = map(int, input().split())
-s = [input() + '#' for _ in range(h)]
-s.append('#' * (w + 1))
-t = [[-1] * w for _ in range(h + 1)]
-tstart = [-1] * (w + 1)
-for i in range(h + 1):
-  ystart = -1
-  for j in range(w + 1):
-    if s[i][j] == '#':
-      if ystart != -1:
-        for k in range(ystart, j):
-          t[i][k] += j - ystart
-        ystart = -1
-      if tstart[j] != -1:
-        for k in range(tstart[j], i):
-          t[k][j] += i - tstart[j]
-        tstart[j] = -1
-    else:
-      if ystart == -1:
-        ystart = j
-      if tstart[j] == -1:
-        tstart[j] = i
-print(max(map(max, t)))
+from sys import stdin
+def main():
+  from builtins import range
+  readline = stdin.readline
+  h, w = map(int, readline().split())
+  s = [readline().rstrip('\r\n') + '#' for _ in range(h)]
+  s.append('#' * w)
+  yoko = [[0] * w for _ in range(h)]
+  for i in range(h):
+    start = -1
+    si = s[i]
+    yokoi = yoko[i]
+    for j in range(w + 1):
+      if si[j] == '#':
+        if start != -1:
+          t = j - start
+          for k in range(start, j):
+            yokoi[k] = t
+          start = -1
+      else:
+        if start == -1:
+          start = j
+  result = 0
+  for i in range(w):
+    start = -1
+    for j in range(h + 1):
+      if s[j][i] == '#':
+        if start != -1:
+          t = yoko_max + j - start - 1
+          if t > result:
+            result = t
+          start = -1
+      else:
+        yji = yoko[j][i]
+        if start == -1:
+          start = j
+          yoko_max = yji
+        else:
+          if yji > yoko_max:
+            yoko_max = yji
+  print(result)
+main()
