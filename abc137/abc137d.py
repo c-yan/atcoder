ce4572adcec70b51997d20dc64066666a9cddeaf
c-yan <c-yan@users.noreply.github.com>
2019-08-22 02:54:15 +0900

    高速化

--- abc137/abc137d.py
+++ abc137/abc137d.py
@@ -1,22 +1,27 @@
-from heapq import heappush, heappop
-n, m = map(int, input().split())
-jobs = {}
-for _ in range(n):
-  a, b = map(int, input().split())
-  if a > m:
-    continue
-  if a in jobs:
-    jobs[a].append(b)
-  else:
-    jobs[a] = [b]
-result = 0
-candidates = []
-for a in range(1, m + 1):
-  if a in jobs:
-    for b in jobs[a]:
-      heappush(candidates, -b)
-  else:
-    if len(candidates) == 0:
+from sys import stdin
+def main():
+  from builtins import int, map, range
+  readline = stdin.readline
+  from heapq import heappush, heappop
+  n, m = map(int, readline().split())
+  jobs = {}
+  for _ in range(n):
+    a, b = map(int, readline().split())
+    if a > m:
       continue
-  result += -heappop(candidates)
-print(result)
+    if a in jobs:
+      jobs[a].append(b)
+    else:
+      jobs[a] = [b]
+  result = 0
+  candidates = []
+  for a in range(1, m + 1):
+    if a in jobs:
+      for b in jobs[a]:
+        heappush(candidates, -b)
+    else:
+      if len(candidates) == 0:
+        continue
+    result += -heappop(candidates)
+  print(result)
+main()
