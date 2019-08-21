b21503fe9d3a62c54a810fec316f59adbb7359af
c-yan <c-yan@users.noreply.github.com>
2019-08-22 01:10:21 +0900

    abc099c

---
+++ abc099/abc099c-2.py
@@ -0,0 +1,19 @@
+# 貰うDP
+n = int(input())
+a = [1]
+i = 6
+while i <= n:
+  a.append(i)
+  i *= 6
+i = 9
+while i <= n:
+  a.append(i)
+  i *= 9
+t = [0] * (n + 1)
+for i in range(1, n + 1):
+  k = float("inf")
+  for j in a:
+    if i - j >= 0 and k > t[i - j] + 1:
+      k = t[i - j] + 1
+  t[i] = k
+print(t[n])
