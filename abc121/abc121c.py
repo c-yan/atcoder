101e7354ce2561a01a5096ba9b790dba02f1d1e2
c-yan <c-yan@users.noreply.github.com>
2019-08-24 00:16:10 +0900

    abc121c

---
+++ abc121/abc121c.py
@@ -0,0 +1,17 @@
+from sys import stdin
+def main():
+  from builtins import int, list, map, min
+  readline = stdin.readline
+  n, m = map(int, readline().split())
+  data = [list(map(int, readline().split())) for _ in range(n)]
+  data.sort(key = lambda x: x[0])
+  purchased = 0
+  amount = 0
+  for a, b in data:
+    t = min(m - purchased, b)
+    amount += t * a
+    purchased += t
+    if purchased == m:
+      break
+  print(amount)
+main()
