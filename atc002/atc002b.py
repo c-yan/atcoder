c553d827d47a46e5763adda50bac16eda751090d
c-yan <c-yan@users.noreply.github.com>
2019-08-25 18:43:37 +0900

    atc002b

---
+++ atc002/atc002b.py
@@ -0,0 +1,11 @@
+n, m, p = map(int, input().split())
+result = 1
+n %= m
+t = n
+result = 1
+while p != 0:
+  if p % 2 == 1:
+    result = (result * t) % m
+  p //= 2
+  t = (t * t) % m
+print(result)
