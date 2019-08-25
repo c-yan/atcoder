ad0420fd936779ac98739e624116f63b90bb657f
c-yan <c-yan@users.noreply.github.com>
2019-08-25 11:36:19 +0900

    abc058c

---
+++ abc058/abc058c.py
@@ -0,0 +1,10 @@
+n = int(input())
+u = [float('inf')] * 26
+for _ in range(n):
+  s = input()
+  t = [0] * 26
+  for i in range(len(s)):
+    t[ord(s[i]) - ord('a')] += 1
+  for i in range(26):
+    u[i] = min(u[i], t[i])
+print(''.join(chr(i + ord('a')) * u[i] for i in range(26)))
