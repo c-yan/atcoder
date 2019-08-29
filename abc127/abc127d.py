7fe5fa6544780d68badddd3eff5c4d6db81cc899
c-yan <c-yan@users.noreply.github.com>
2019-08-29 23:38:25 +0900

    cmp_to_key を使う必要がないことに気づいたので修正するついでに、他の気になるところも手を入れた

--- abc127/abc127d.py
+++ abc127/abc127d.py
@@ -1,8 +1,7 @@
-from functools import cmp_to_key
 n, m = map(int, input().split())
-a = [int(e) for e in input().split()]
-bc = [[int(e) for e in input().split()] for _ in range(m)]
-bc.sort(key = cmp_to_key(lambda x, y: y[1] - x[1]))
+a = list(map(int, input().split()))
+bc = [list(map(int, input().split())) for _ in range(m)]
+bc.sort(key = lambda x: x[1], reverse = True)
 t = 0
 for b, c in bc:
   a.extend([c] * b)
