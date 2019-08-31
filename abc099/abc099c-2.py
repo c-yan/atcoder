7cf5823664c30d7edfedd3c19af9be1c457697df
c-yan <c-yan@users.noreply.github.com>
2019-09-01 01:20:30 +0900

    ループの中で繰り返し float('inf') を実行しているのを修正

--- abc099/abc099c-2.py
+++ abc099/abc099c-2.py
@@ -1,4 +1,5 @@
 # 貰うDP
+INF = float('inf')
 n = int(input())
 a = [1]
 i = 6
@@ -11,7 +12,7 @@ while i <= n:
   i *= 9
 t = [0] * (n + 1)
 for i in range(1, n + 1):
-  k = float('inf')
+  k = INF
   for j in a:
     if i - j >= 0 and k > t[i - j] + 1:
       k = t[i - j] + 1
