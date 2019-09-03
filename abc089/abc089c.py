e14908374be07a6c0aa2f741d134ac58f9284332
c-yan <c-yan@users.noreply.github.com>
2019-09-03 10:11:59 +0900

    abc089c

---
+++ abc089/abc089c.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = [input() for _ in range(n)]
+s = [t[0] for t in s if t[0] in 'MARCH']
+d = {}
+for t in s:
+  if t in d:
+    d[t] += 1
+  else:
+    d[t] = 1
+l = list(d.keys())
+result = 0
+for i in range(len(l) - 2):
+  for j in range(i + 1, len(l) - 1):
+    for k in range(j + 1, len(l)):
+      result += d[l[i]] * d[l[j]] * d[l[k]]
+print(result)
