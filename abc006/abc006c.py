d1f39f2671bf087774986f2c9eaacadea35b1835
c-yan <c-yan@users.noreply.github.com>
2019-09-06 02:37:28 +0900

    abc006a, abc006b, abc006c

---
+++ abc006/abc006c.py
@@ -0,0 +1,12 @@
+from sys import exit
+N, M = map(int, input().split())
+if M > 4 * N or M < 2 * N:
+  print("-1 -1 -1")
+  exit()
+t = (M - 2 * N) // 2
+a = N - t
+c = t
+t = M - 2 * a - 4 * c
+a -= t
+b = t
+print(*[a, b, c])
