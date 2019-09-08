a9974f0d99ec15c285321a50e256f4d434fcdbe2
c-yan <c-yan@users.noreply.github.com>
2019-09-08 15:30:23 +0900

    最初に 0-indexed 化するよう変更

--- abc140/abc140b.py
+++ abc140/abc140b.py
@@ -1,12 +1,12 @@
 N = int(input())
-A = list(map(int, input().split()))
+A = [int(e) - 1 for e in input().split()]
 B = list(map(int, input().split()))
 C = list(map(int, input().split()))
 before = N
 result = 0
 for i in range(N):
-  result += B[A[i] - 1]
-  if before + 1 == A[i] - 1:
+  result += B[A[i]]
+  if before + 1 == A[i]:
     result += C[before]
-  before = A[i] - 1
+  before = A[i]
 print(result)
