e70a2226384826b7ad8552bbba22b2e2b76a9110
c-yan <c-yan@users.noreply.github.com>
2019-09-09 19:59:22 +0900

    abc005c

---
+++ abc005/abc005c.py
@@ -0,0 +1,17 @@
+from sys import exit
+T = int(input())
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+i = 0
+for b in B:
+  while i < N:
+    a = A[i] 
+    i += 1
+    if a <= b and a + T >= b:
+      break
+  else:
+    print('no')
+    exit()
+print('yes')
