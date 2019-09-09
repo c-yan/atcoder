32b2435ed7a749384a489c4531959e03ae2be592
c-yan <c-yan@users.noreply.github.com>
2019-09-10 01:51:23 +0900

    abc068c

---
+++ abc068/abc058c.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+from1 = set()
+toN = set()
+for _ in range(M):
+  a, b = map(int, input().split())
+  if a == 1:
+    from1.add(b)
+  elif b == N:
+    toN.add(a)
+if from1 & toN:
+  print('POSSIBLE')
+else:
+  print('IMPOSSIBLE')
