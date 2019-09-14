188b4276bf43fdc144fdd13914e67890585adad7
c-yan <c-yan@users.noreply.github.com>
2019-09-15 00:24:15 +0900

    abc110c

---
+++ abc110/abc110c.py
@@ -0,0 +1,15 @@
+S = input()
+T = input()
+
+d = {}
+for i in range(len(S)):
+  d[S[i]] = T[i]
+
+t = []
+for i in range(len(S)):
+  t.append(d[S[i]])
+
+if ''.join(t) != T or len(d.values()) != len(set(d.values())):
+  print('No')
+else:
+  print('Yes')
