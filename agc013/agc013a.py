8cb73a872b345ae0474b7dab6f8647c872e08ab9
c-yan <c-yan@users.noreply.github.com>
2019-09-20 00:02:36 +0900

    agc013a

---
+++ agc013/agc013a.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = list(map(int, input().split()))
+result = 1
+d = 0
+for i in range(1, N):
+  if A[i - 1] == A[i]:
+    continue
+  if d == 0:
+    if A[i - 1] > A[i]:
+      d = -1
+    else:
+      d = 1
+  elif d == 1 and A[i - 1] > A[i]:
+    d = 0
+    result += 1
+  elif d == -1 and A[i - 1] < A[i]:
+    d = 0
+    result += 1
+print(result)
