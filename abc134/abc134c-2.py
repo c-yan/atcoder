e0e443c47e76d9990a8f3f17a4adc71efad08491
c-yan <c-yan@users.noreply.github.com>
2019-09-21 08:46:50 +0900

    abc134c 累積和は一般的に cumulative sum であって accumulative sum ではないようなので変数名を修正

--- abc134/abc134c-2.py
+++ abc134/abc134c-2.py
@@ -1,13 +1,13 @@
 # 累積和(max)
 N = int(input())
 a = [int(input()) for _ in range(N)]
-lam = a[:]
-ram = a[:]
+lcm = a[:]
+rcm = a[:]
 for i in range(1, N):
-  lam[i] = max(lam[i], lam[i - 1])
+  lcm[i] = max(lcm[i], lcm[i - 1])
 for i in range(N - 2, -1, -1):
-  ram[i] = max(ram[i], ram[i + 1])
-print(ram[1])
+  rcm[i] = max(rcm[i], rcm[i + 1])
+print(rcm[1])
 for i in range(1, N - 1):
-  print(max(lam[i - 1], ram[i + 1]))
-print(lam[N - 2])
+  print(max(lcm[i - 1], rcm[i + 1]))
+print(lcm[N - 2])
