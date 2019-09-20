ce1d0747fdcc3cad210a9f00e52d1ffbd5a4f918
c-yan <c-yan@users.noreply.github.com>
2019-09-21 08:42:35 +0900

    abc084d 不要な最適化の除去、使用手法の追記、インデックスずれの修正、問題文に合わせた変数名の大文字化、最大値変数の導入

--- abc084/abc084d.py
+++ abc084/abc084d.py
@@ -1,24 +1,27 @@
-from sys import stdin
-def main():
-  from math import sqrt
-  readline = stdin.readline
-  p = [True] * (10 ** 5 + 1)
-  p[0] = False
-  p[1] = False
-  n = int(sqrt(10 ** 5)) + 1
-  for j in range(4, 10 ** 5 + 1, 2):
-    p[j] = False
-  for i in range(3, n + 1, 2):
-    if p[i]:
-      for j in range(i + i, 10 ** 5 + 1, i):
-        p[j] = False
-  c = [0] * (10 ** 5 + 2)
-  for i in range(2, 10 ** 5 + 2):
-    c[i] = c[i - 1]
-    if p[i - 2] and p[(i - 1) // 2]:
-      c[i] += 1
-  q = int(readline())
-  for _ in range(q):
-    l, r = map(int, readline().split())
-    print(c[r + 2] - c[l + 1])
-main()
+# エラトステネスの篩, 累積和
+from math import sqrt
+
+N = 10 ** 5
+
+p = [True] * (N + 1)
+p[0] = False
+p[1] = False
+n = int(sqrt(N) + 1)
+for j in range(4, N + 1, 2):
+  p[j] = False
+for i in range(3, n + 1, 2):
+  if p[i]:
+    for j in range(i + i, N + 1, i):
+      p[j] = False
+
+cs = [0] * (N + 1)
+for i in range(3, N + 1, 2):
+  if p[i] and p[(i + 1) // 2]:
+    cs[i] = 1
+for i in range(1, N + 1):
+  cs[i] += cs[i - 1]
+
+Q = int(input())
+for _ in range(Q):
+  l, r = map(int, input().split())
+  print(cs[r] - cs[l - 1])
