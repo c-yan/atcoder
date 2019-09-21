cf5ba9f72c957645664ed5b7a89e3668235c3aeb
c-yan <c-yan@users.noreply.github.com>
2019-09-21 22:56:59 +0900

    abc069c 変数名をスネークケースに修正

--- abc069/abc069c.py
+++ abc069/abc069c.py
@@ -1,12 +1,12 @@
 N = int(input())
-oddCount = 0
-mo4Count = 0
+odd_count = 0
+mo4_count = 0 # multiple of 4
 for i in map(int, input().split()):
   if i % 2 == 1:
-    oddCount += 1
+    odd_count += 1
   elif i % 4 == 0:
-    mo4Count += 1
-if oddCount - N % 2 <= mo4Count:
+    mo4_count += 1
+if odd_count - N % 2 <= mo4_count:
   print('Yes')
 else:
   print('No')
