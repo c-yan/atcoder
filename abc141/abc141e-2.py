572a800faf85fedf3ea862ce6f9b47d90fb31527
c-yan <c-yan@users.noreply.github.com>
2019-09-22 20:55:46 +0900

    ab141e 使用手法の追記、無駄な代入の削除

--- abc141/abc141e-2.py
+++ abc141/abc141e-2.py
@@ -1,13 +1,12 @@
 # PyPy なら通る
+# DP(貰うDP)
 N = int(input())
 S = input()
 
 dp = [[0] * (N + 1) for _ in range(N + 1)]
 for i in range(N - 1, -1, -1):
     for j in range(N - 1, -1, -1):
-        if S[i] != S[j]:
-            dp[i][j] = 0
-        else:
+        if S[i] == S[j]:
             dp[i][j] = dp[i + 1][j + 1] + 1
 
 result = 0
