4bdb0415da0628cd8a22fce11f53d118e7feb64a
c-yan <c-yan@users.noreply.github.com>
2019-09-22 23:21:55 +0900

    dpd 書き直し

--- dp/dpd.py
+++ dp/dpd.py
@@ -1,21 +1,17 @@
-from sys import stdin
-
-
+# DP
 def main():
-    from builtins import int, range
-    readline = stdin.readline
-    n, w = map(int, readline().split())
-    t = [-1] * (w + 1)
-    t[w] = 0
-    for _ in range(n):
-        nw, nv = map(int, readline().split())
-        for i in range(nw, w + 1):
-            ti = t[i]
-            if ti == -1:
-                continue
-            if t[i - nw] < ti + nv:
-                t[i - nw] = ti + nv
-    print(max(t))
+    N, W = map(int, input().split())
+
+    dp = [-1] * (W + 1)
+    dp[0] = 0
+    for _ in range(N):
+        w, v = map(int, input().split())
+        for i in range(W - w, -1, -1):
+            dpi = dp[i]
+            if dpi != -1:
+                if dp[i + w] < dpi + v:
+                    dp[i + w] = dpi + v
+    print(max(dp))
 
 
 main()
