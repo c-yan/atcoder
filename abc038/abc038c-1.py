9d38a0333923700a264bbeb9f9d9bf840e8f80aa
c-yan <c-yan@users.noreply.github.com>
2019-09-26 20:57:03 +0900

    abc038c 別解

--- abc038/abc038c-1.py
+++ abc038/abc038c-1.py
@@ -3,10 +3,10 @@ a = list(map(int, input().split()))
 result = 0
 start = 0
 for i in range(N - 1):
-  if a[i] >= a[i + 1]:
-    t = i - start + 1
-    result += (t + 1) * t // 2
-    start = i + 1
+    if a[i] >= a[i + 1]:
+        t = i - start + 1
+        result += (t + 1) * t // 2
+        start = i + 1
 t = (N - 1) - start + 1
 result += (t + 1) * t // 2
 print(result)
---
+++ abc038/abc038c-2.py
@@ -0,0 +1,8 @@
+# 累積和
+N = int(input())
+a = list(map(int, input().split()))
+cs = [1] * N
+for i in range(1, N):
+    if a[i] > a[i - 1]:
+        cs[i] += cs[i - 1]
+print(sum(cs))
