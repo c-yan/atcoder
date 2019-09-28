1bb370e188d95acc4518125b22c1c932fdf768af
c-yan <c-yan@users.noreply.github.com>
2019-09-29 08:34:45 +0900

    abc142c 整理

--- abc142/abc142c.py
+++ abc142/abc142c.py
@@ -3,7 +3,4 @@ from operator import itemgetter
 N = int(input())
 A = list(map(int, input().split()))
 
-x = [(A[i - 1], i) for i in range(1, N + 1)]
-x.sort(key=itemgetter(0))
-y = [t[1] for t in x]
-print(*y)
+print(*[t[0] + 1 for t in sorted(enumerate(A), key=itemgetter(1))])
