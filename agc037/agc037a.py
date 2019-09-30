aac99c9929f8d735297729c7ea886e0af04da473
c-yan <c-yan@users.noreply.github.com>
2019-09-30 20:09:50 +0900

    agc037a 書き直し、分割文字列そのものをちゃんと生成するようにした

--- agc037/agc037a.py
+++ agc037/agc037a.py
@@ -1,11 +1,19 @@
-s = input()
-t = ''
-k = 0
-prev = ''
-for i in range(len(s)):
-    t += s[i]
-    if prev != t:
-        k += 1
-        prev = t
-        t = ''
-print(k)
+def divide_string(s):
+    result = []
+    prev, t = '', ''
+    for i in range(len(s)):
+        t += s[i]
+        if prev != t:
+            result.append(t)
+            prev, t = t, ''
+    if t != '':
+        if len(result) == 1 or len(result[-2]) == 1:
+            result[-1] = result[-1] + t
+        else:
+            result[-2] = result[-2] + t
+    return result
+
+
+S = input()
+
+print(len(divide_string(S)))
