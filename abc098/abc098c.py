76049a346263b46c210957402578556c1e9c9726
c-yan <c-yan@users.noreply.github.com>
2019-10-04 20:21:22 +0900

    abc098c 書き直し、問題文に合わせた変数名の大文字化、使用手法の追記

--- abc098/abc098c.py
+++ abc098/abc098c.py
@@ -1,14 +1,17 @@
-n = int(input())
-s = input()
-was = [0] * n
-if s[0] == 'W':
-  was[0] = 1
-for i in range(1, n):
-  if s[i] == 'W':
-    was[i] = was[i - 1] + 1
-  else:
-    was[i] = was[i - 1]
-result = (n - 1) - (was[n - 1] - was[0])
-for i in range(1, n):
-  result = min(result, was[i - 1] + ((n - (i + 1)) - (was[n - 1] - was[i])))
-print(result)
+# 累積和
+N = int(input())
+S = input()
+
+cws = [0] * N  # i番目がリーダーのとき、それより左にいる西を向いている人数
+for i in range(1, N):
+    if S[i - 1] == 'W':
+        cws[i] = 1
+    cws[i] += cws[i - 1]
+
+ces = [0] * N  # i番目がリーダーのとき、それより右にいる東を向いている人数
+for i in range(N - 2, -1, -1):
+    if S[i + 1] == 'E':
+        ces[i] = 1
+    ces[i] += ces[i + 1]
+
+print(min(cws[i] + ces[i] for i in range(N)))
