510e43b118b3850bf8ea03cf965d59e74d4ccc03
c-yan <c-yan@users.noreply.github.com>
2019-10-07 21:02:09 +0900

    abc049d 不要な最適化の除去

--- abc049/abc049d-1.py
+++ abc049/abc049d-1.py
@@ -1,48 +1,41 @@
 # Union Find 木
-from sys import stdin
+def find(parent, i):
+    t = parent[i]
+    if t < 0:
+        return i
+    t = find(parent, t)
+    parent[i] = t
+    return t
 
 
-def main():
-    def find(parent, i):
-        t = parent[i]
-        if t == -1:
-            return i
-        t = find(parent, t)
-        parent[i] = t
-        return t
-
-    def unite(parent, i, j):
-        i = find(parent, i)
-        j = find(parent, j)
-        if i == j:
-            return
-        parent[i] = j
-
-    from builtins import int, map, range
-    readline = stdin.readline
-
-    N, K, L = map(int, readline().split())
-
-    roads = [-1] * N
-    rails = [-1] * N
-
-    for i in range(K):
-        p, q = map(int, readline().split())
-        unite(roads, p - 1, q - 1)
-
-    for i in range(L):
-        r, s = map(int, readline().split())
-        unite(rails, r - 1, s - 1)
-
-    d = {}
-    for i in range(N):
-        t = (find(roads, i), find(rails, i))
-        if t in d:
-            d[t] += 1
-        else:
-            d[t] = 1
-
-    print(*[d[(find(roads, i), find(rails, i))] for i in range(N)])
+def unite(parent, i, j):
+    i = find(parent, i)
+    j = find(parent, j)
+    if i == j:
+        return
+    parent[j] += parent[i]
+    parent[i] = j
 
 
-main()
+N, K, L = map(int, input().split())
+
+roads = [-1] * N
+rails = [-1] * N
+
+for i in range(K):
+    p, q = map(int, input().split())
+    unite(roads, p - 1, q - 1)
+
+for i in range(L):
+    r, s = map(int, input().split())
+    unite(rails, r - 1, s - 1)
+
+d = {}
+for i in range(N):
+    t = (find(roads, i), find(rails, i))
+    if t in d:
+        d[t] += 1
+    else:
+        d[t] = 1
+
+print(*[d[(find(roads, i), find(rails, i))] for i in range(N)])
