b88f770df2a5cec24e1ca74fa3defd2174e3be31
c-yan <c-yan@users.noreply.github.com>
2019-10-09 01:38:36 +0900

    agc019a レイアウト調整、関数の引数に料金を追加

--- agc019/agc019a.py
+++ agc019/agc019a.py
@@ -1,13 +1,12 @@
+def f(n, l, price):
+    if l == 0.25:
+        return int(n / 0.25 * price[l])
+    t = int(n / l)
+    return min(f(n - l * t, l / 2, price) + t * price[l], f(n, l / 2, price))
+
+
 Q, H, S, D = map(int, input().split())
-price = {0.25: Q, 0.5: H, 1.0: S, 2.0: D}
 N = int(input())
 
-
-def f(n, l):
-    if l == 0.25:
-        return int(n / 0.25) * price[l]
-    t = int(n / l)
-    return min(f(n - l * t, l / 2) + t * price[l], f(n, l / 2))
-
-
-print(int(f(N, 2.0)))
+price = {0.25: Q, 0.5: H, 1.0: S, 2.0: D}
+print(f(N, 2.0, price))
