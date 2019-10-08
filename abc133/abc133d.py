d21475631bd09aabe6003e62826f79f39b6c3949
c-yan <c-yan@users.noreply.github.com>
2019-10-09 00:42:02 +0900

    abc133d 問題文に合わせた変数名の大文字化、塊ごとに区切るために空行を入れた、入力の正規化、事前にリストを作ってしまうよう変更

--- abc133/abc133d.py
+++ abc133/abc133d.py
@@ -1,6 +1,8 @@
-n = int(input())
-a = [int(e) for e in input().split()]
-result = [sum(a[::2])-sum(a[1::2])]
-for i in range(len(a) - 1):
-    result.append(2*a[i]-result[i])
-print(' '.join(map(str, result)))
+N = int(input())
+A = list(map(int, input().split()))
+
+t = [0] * N
+t[0] = sum(A[::2]) - sum(A[1::2])
+for i in range(1, N):
+    t[i] = 2 * A[i - 1] - t[i - 1]
+print(*list(map(str, t)))
