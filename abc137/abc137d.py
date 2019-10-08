e4bed7b225076178618f920f5ddbdcb4d739c133
c-yan <c-yan@users.noreply.github.com>
2019-10-09 00:40:14 +0900

    abc137d 不要な最適化の除去、問題文に合わせた変数名の大文字化、塊ごとに区切るために空行を入れた、不要な if 文によるチェックを削除

--- abc137/abc137d.py
+++ abc137/abc137d.py
@@ -1,32 +1,24 @@
 # 優先度付きキュー
-from sys import stdin
+from heapq import heappush, heappop
 
+N, M = map(int, input().split())
 
-def main():
-    from builtins import int, map, range
-    readline = stdin.readline
-    from heapq import heappush, heappop
-    n, m = map(int, readline().split())
-    jobs = {}
-    for _ in range(n):
-        a, b = map(int, readline().split())
-        if a > m:
+jobs = {}
+for _ in range(N):
+    A, B = map(int, input().split())
+    if A in jobs:
+        jobs[A].append(B)
+    else:
+        jobs[A] = [B]
+
+result = 0
+candidates = []
+for a in range(1, M + 1):
+    if a in jobs:
+        for b in jobs[a]:
+            heappush(candidates, -b)
+    else:
+        if len(candidates) == 0:
             continue
-        if a in jobs:
-            jobs[a].append(b)
-        else:
-            jobs[a] = [b]
-    result = 0
-    candidates = []
-    for a in range(1, m + 1):
-        if a in jobs:
-            for b in jobs[a]:
-                heappush(candidates, -b)
-        else:
-            if len(candidates) == 0:
-                continue
-        result += -heappop(candidates)
-    print(result)
-
-
-main()
+    result += -heappop(candidates)
+print(result)
