4ca2bab8863556fe9c43bdfe99732f965a42a59f
c-yan <c-yan@users.noreply.github.com>
2019-10-09 00:49:59 +0900

    abc141c 変数名の変更

--- abc141/abc141c-2.py
+++ abc141/abc141c-2.py
@@ -1,22 +1,25 @@
 # imos 法
 N, K, Q = map(int, input().split())
-cs = [0] * N
-cs[0] = K
+
+score = [0] * N
+score[0] = K
 for _ in range(Q):
     A = int(input())
     if A == 1:
-        cs[1] -= 1
+        score[1] -= 1
     elif A == N:
-        cs[0] -= 1
-        cs[N - 1] += 1
+        score[0] -= 1
+        score[N - 1] += 1
     else:
-        cs[0] -= 1
-        cs[A - 1] += 1
-        cs[A] -= 1
+        score[0] -= 1
+        score[A - 1] += 1
+        score[A] -= 1
+
 for i in range(1, N):
-    cs[i] += cs[i - 1]
-for i in cs:
-    if i > 0:
+    score[i] += score[i - 1]
+
+for v in score:
+    if v > 0:
         print('Yes')
     else:
         print('No')
