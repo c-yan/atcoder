e40b337af0e4d478014415e20158c2b6f9fabc51
c-yan <c-yan@users.noreply.github.com>
2019-10-09 01:36:12 +0900

    agc023a 問題文に合わせた変数名の大文字化、塊ごとに区切るために空行を入れた、辞書のカウントアップの正規化

--- agc023/agc023a.py
+++ agc023/agc023a.py
@@ -1,13 +1,17 @@
-n = int(input())
-a = list(map(int, input().split()))
-c = [0] * (n + 1)
-for i in range(n):
-    c[i + 1] = c[i] + a[i]
+N = int(input())
+A = list(map(int, input().split()))
+
+c = [0] * (N + 1)
+for i in range(N):
+    c[i + 1] = c[i] + A[i]
+
 d = {}
-for i in range(1, n + 1):
+for i in range(1, N + 1):
     if c[i] not in d:
-        d[c[i]] = 0
-    d[c[i]] += 1
+        d[c[i]] = 1
+    else:
+        d[c[i]] += 1
+
 result = 0
 if 0 in d:
     result += d[0]
