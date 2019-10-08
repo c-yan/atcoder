eb3493ddaffbd0c0b5764adc9fc10a2004fa5326
c-yan <c-yan@users.noreply.github.com>
2019-10-09 01:43:45 +0900

    abc113c 不要な最適化の除去、問題文に合わせた変数名の大文字化、塊ごとに区切るために空行を入れた、変数名の変更

--- abc113/abc113c.py
+++ abc113/abc113c.py
@@ -1,18 +1,15 @@
 # 二分探索
-def main():
-    from sys import stdin
-    from bisect import bisect_left
-    readline = stdin.readline
-    n, m = map(int, readline().split())
-    data = [list(map(int, readline().split())) for _ in range(m)]
-    t = [[] for _ in range(n)]
-    for p, y in data:
-        t[p - 1].append(y)
-    for i in range(n):
-        t[i].sort()
-    for p, y in data:
-        nth = bisect_left(t[p - 1], y) + 1
-        print('%06d%06d' % (p, nth))
+from bisect import bisect_left
 
+N, M = map(int, input().split())
+PY = [list(map(int, input().split())) for _ in range(M)]
 
-main()
+t = [[] for _ in range(N)]
+for P, Y in PY:
+    t[P - 1].append(Y)
+
+for i in range(N):
+    t[i].sort()
+
+for P, Y in PY:
+    print("%06d%06d" % (P, bisect_left(t[P - 1], Y) + 1))
