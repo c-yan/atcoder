fc481fb9af6babe902e2dd0dab12b6c85b4d7a90
c-yan <c-yan@users.noreply.github.com>
2019-11-05 08:22:22 +0900

    abc080d 最大チャンネル数を有効活用していなかったので修正

--- abc080/abc080d-1.py
+++ abc080/abc080d-1.py
@@ -1,6 +1,6 @@
 N, C = map(int, input().split())
 
-tt = [[0] * (10 ** 5 + 1) for _ in range(30)]
+tt = [[0] * (10 ** 5 + 1) for _ in range(C)]
 for _ in range(N):
     s, t, c = map(int, input().split())
     ttc = tt[c - 1]
@@ -8,7 +8,7 @@ for _ in range(N):
         ttc[i] = 1
 
 ct = [0] * (10 ** 5 + 1)
-for i in range(30):
+for i in range(C):
     tti = tt[i]
     for j in range(10 ** 5 + 1):
         ct[j] += tti[j]
