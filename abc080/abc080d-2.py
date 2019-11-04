918b94dc88e342cee74ea768361b3e3dd8715cc2
c-yan <c-yan@users.noreply.github.com>
2019-11-05 06:17:44 +0900

    abc080d 2倍にする必要がなかったので修正

--- abc080/abc080d-2.py
+++ abc080/abc080d-2.py
@@ -5,20 +5,20 @@ N, C = map(int, input().split())
 stc = [list(map(int, input().split())) for _ in range(N)]
 stc.sort(key=itemgetter(2, 0))
 
-cs = [0] * (10 ** 5 * 2 + 1)
+cs = [0] * (10 ** 5 + 1)
 pc = -1
 for s, t, c in stc:
     if pc != c:
         pt = -1
         pc = c
     if pt == s:
-        cs[s * 2] += 1
+        cs[s] += 1
     else:
-        cs[s * 2 - 1] += 1
-    cs[t * 2] -= 1
+        cs[s - 1] += 1
+    cs[t] -= 1
     pt = t
 
-for i in range(1, 10 ** 5 * 2 + 1):
+for i in range(1, 10 ** 5 + 1):
     cs[i] += cs[i - 1]
 
 print(max(cs))
