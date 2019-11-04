accf33468069545522e7e99740f13f11957ba504
c-yan <c-yan@users.noreply.github.com>
2019-11-05 08:26:27 +0900

    abc080d 必要のない最適化をしていたので修正

--- abc080/abc080d-1.py
+++ abc080/abc080d-1.py
@@ -3,14 +3,12 @@ N, C = map(int, input().split())
 tt = [[0] * (10 ** 5 + 1) for _ in range(C)]
 for _ in range(N):
     s, t, c = map(int, input().split())
-    ttc = tt[c - 1]
     for i in range(s - 1, t):
-        ttc[i] = 1
+        tt[c - 1][i] = 1
 
 ct = [0] * (10 ** 5 + 1)
 for i in range(C):
-    tti = tt[i]
     for j in range(10 ** 5 + 1):
-        ct[j] += tti[j]
+        ct[j] += tt[i][j]
 
 print(max(ct))
