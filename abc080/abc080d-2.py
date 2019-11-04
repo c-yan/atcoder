5426194ac0729c35177f61e3f6aba57884671f2c
c-yan <c-yan@users.noreply.github.com>
2019-11-05 01:16:13 +0900

    abc080d 別解(imos 法)

---
+++ abc080/abc080d-2.py
@@ -0,0 +1,24 @@
+# imos 法
+from operator import itemgetter
+
+N, C = map(int, input().split())
+stc = [list(map(int, input().split())) for _ in range(N)]
+stc.sort(key=itemgetter(2, 0))
+
+cs = [0] * (10 ** 5 * 2 + 1)
+pc = -1
+for s, t, c in stc:
+    if pc != c:
+        pt = -1
+        pc = c
+    if pt == s:
+        cs[s * 2] += 1
+    else:
+        cs[s * 2 - 1] += 1
+    cs[t * 2] -= 1
+    pt = t
+
+for i in range(1, 10 ** 5 * 2 + 1):
+    cs[i] += cs[i - 1]
+
+print(max(cs))
