2ef0bfc4a89e11467c26e0108ea706f208e51797
c-yan <c-yan@users.noreply.github.com>
2019-11-05 09:06:00 +0900

    abc080d 連続判定のコードを行数が少ない形に変更

--- abc080/abc080d-2.py
+++ abc080/abc080d-2.py
@@ -6,17 +6,14 @@ stc = [list(map(int, input().split())) for _ in range(N)]
 stc.sort(key=itemgetter(2, 0))
 
 cs = [0] * (10 ** 5 + 1)
-pc = -1
+pt, pc = -1, -1
 for s, t, c in stc:
-    if pc != c:
-        pt = -1
-        pc = c
-    if pt == s:
+    if pt == s and pc == c:
         cs[s] += 1
     else:
         cs[s - 1] += 1
     cs[t] -= 1
-    pt = t
+    pt, pc = t, c
 
 for i in range(1, 10 ** 5 + 1):
     cs[i] += cs[i - 1]
