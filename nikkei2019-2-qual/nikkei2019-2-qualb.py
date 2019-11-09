3edabee7a5456a646b06a0de2dbd0c78567f4aa2
c-yan <c-yan@users.noreply.github.com>
2019-11-09 23:14:38 +0900

    nikkei2019-2-quala, nikkei2019-2-qualb

---
+++ nikkei2019-2-qual/nikkei2019-2-qualb.py
@@ -0,0 +1,27 @@
+from sys import exit
+
+N = int(input())
+D = list(map(int, input().split()))
+
+if D[0] != 0:
+    print(0)
+    exit()
+
+c = {}
+for i in D:
+    if i in c:
+        c[i] += 1
+    else:
+        c[i] = 1
+
+if c[0] != 1:
+    print(0)
+    exit()
+
+result = 1
+for i in range(1, max(D) + 1):
+    if i not in c:
+        print(0)
+        exit()
+    result = result * (c[i - 1] ** c[i]) % 998244353
+print(result)
