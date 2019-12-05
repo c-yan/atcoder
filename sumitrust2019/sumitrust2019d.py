ae170ef35138fa28daddc8cbafe93e5055a5e56b
c-yan <c-yan@users.noreply.github.com>
2019-12-06 04:05:55 +0900

    sumitrust2019b 圧縮をする必要がなかったので修正

--- sumitrust2019/sumitrust2019d.py
+++ sumitrust2019/sumitrust2019d.py
@@ -1,29 +1,16 @@
 N = int(input())
 S = input()
 
-t = [S[0]]
-p = S[0]
-r = 1
-for i in range(1, N):
-    if S[i] == p:
-        r += 1
-        if r < 4:
-            t.append(S[i])
-    else:
-        r = 1
-        t.append(S[i])
-T = ''.join(t)
-
 result = 0
 for i in range(10):
-    a = T.find(str(i))
-    if a == -1:
-        continue
-    for j in range(10):
-        b = T.find(str(j), a + 1)
-        if b == -1:
-            continue
-        for k in range(10):
-            if T.find(str(k), b + 1) != -1:
-                result += 1
+  a = S.find(str(i))
+  if a == -1:
+    continue
+  for j in range(10):
+    b = S.find(str(j), a + 1)
+    if b == -1:
+      continue
+    for k in range(10):
+      if S.find(str(k), b + 1) != -1:
+        result += 1
 print(result)
