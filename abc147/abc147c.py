fa6c8af6a4ab189523a269c3cbda9dc025b3eaee
c-yan <c-yan@users.noreply.github.com>
2019-12-09 08:07:23 +0900

    abc147c 変数名の改善、テンポラリテーブルの除去

--- abc147/abc147c.py
+++ abc147/abc147c.py
@@ -1,25 +1,24 @@
 N = int(input())
 
-s = [[] for _ in range(N)]
+evidences = [[] for _ in range(N)]
 for i in range(N):
     A = int(input())
     for _ in range(A):
         x, y = map(int, input().split())
-        s[i].append((x - 1, y))
+        evidences[i].append((x - 1, y))
 
 result = 0
 for i in range(1, 2 ** N):
-    t = [-1] * N
-    for j in range(N):
-        t[j] = (i >> j) & 1
-    m = False
+    consistent = True
     for j in range(N):
         if (i >> j) & 1 == 0:
             continue
-        for x, y in s[j]:
-            if t[x] != y:
-                m = True
+        for x, y in evidences[j]:
+            if (i >> x) & 1 != y:
+                consistent = False
                 break
-    if not m:
+        if not consistent:
+            break
+    if consistent:
         result = max(result, bin(i)[2:].count('1'))
 print(result)
