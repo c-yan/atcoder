c9fba2b1e55e03e57bf7966c663e71c2620cdeff
c-yan <c-yan@users.noreply.github.com>
2019-12-10 06:24:16 +0900

    abc147d 別解からのフィードバック

--- abc147/abc147d-1.go
+++ abc147/abc147d-1.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	const m = 1000000007
+
 	N := readInt()
 	A := make([]int, N)
 	for i := 0; i < N; i++ {
@@ -15,29 +17,31 @@ func main() {
 	}
 
 	result := 0
+	a := make([]int, N)
+	bs := make([]int, N)
 	for b := 0; b < 60; b++ {
-		a := make([]int, N)
 		for i := 0; i < N; i++ {
 			a[i] = (A[i] >> uint(b)) & 1
 		}
 
-		bs := make([]int, N)
 		bs[N-1] = a[N-1]
 		for i := N - 2; i > -1; i-- {
 			bs[i] = bs[i+1] + a[i]
 		}
 
+		t := 0
 		for i := 0; i < N-1; i++ {
-			t := 0
 			if a[i] == 0 {
-				t = bs[i+1]
+				t += bs[i+1]
 			} else {
-				t = (N - (i + 1)) - bs[i+1]
+				t += (N - (i + 1)) - bs[i+1]
 			}
-			B := (1 << uint(b)) % 1000000007
-			result += (t * B) % 1000000007
-			result = result % 1000000007
 		}
+		t %= m
+
+		v := (1 << uint(b)) % m
+		result += (t * v) % m
+		result %= m
 	}
 	fmt.Println(result)
 }
