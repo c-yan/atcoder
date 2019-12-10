e50f113d66307aaffb74640a80cfb96150a11a4b
c-yan <c-yan@users.noreply.github.com>
2019-12-11 01:36:17 +0900

    abc109b

---
+++ abc109/abc109b.py
@@ -0,0 +1,14 @@
+from sys import exit
+
+N = int(input())
+
+prev = input()
+used = set([prev])
+for _ in range(N - 1):
+    s = input()
+    if s in used or prev[-1] != s[0]:
+        print('No')
+        exit()
+    prev = s
+    used.add(s)
+print('Yes')
