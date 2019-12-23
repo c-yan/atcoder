1836cfe773ea4865fa66f30528339b27711ffad1
c-yan <c-yan@users.noreply.github.com>
2019-12-23 20:24:29 +0900

    abc144c exit のインポートが不要だったので削除

--- abc144/abc144c.py
+++ abc144/abc144c.py
@@ -1,9 +1,8 @@
 from math import sqrt
-from sys import exit
 
 N = int(input())
 
 for i in range(int(sqrt(N)) + 1, -1, -1):
     if N % i == 0:
         print((i - 1) + (N // i - 1))
-        exit()
+        break
