4d62b6633a5464083cc111524e28b448ce6ad1b5
c-yan <c-yan@users.noreply.github.com>
2019-12-24 00:10:28 +0900

    abc148f

---
+++ abc148/abc148f.py
@@ -0,0 +1,40 @@
+from sys import exit
+
+N, u, v = map(int, input().split())
+
+if u == v:
+    print(0)
+    exit()
+
+edges = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    A, B = map(int, input().split())
+    edges[A].append(B)
+    edges[B].append(A)
+
+
+def calc_destination(start, edges):
+    destination = [-1] * (N + 1)
+    destination[start] = 0
+    q = [start]
+    while len(q) != 0:
+        current = q.pop()
+        for n in edges[current]:
+            if destination[n] != -1:
+                continue
+            destination[n] = destination[current] + 1
+            q.append(n)
+    return destination
+
+
+tak = calc_destination(u, edges)
+aok = calc_destination(v, edges)
+
+result = 0
+for i in range(1, N + 1):
+    aoki = aok[i]
+    if tak[i] >= aoki:
+        continue
+    if aoki > result:
+        result = aoki
+print(result - 1)
