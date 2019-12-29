5aacc28f8ab9d9fce6a4ef8fca9f6b4541955c72
c-yan <c-yan@users.noreply.github.com>
2019-12-30 08:43:33 +0900

    abc098d Python版

---
+++ abc098/abc098d.py
@@ -0,0 +1,16 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+result = 0
+r = 0
+xs = A[0]
+cs = A[0]
+for l in range(N):
+    while r < N - 1 and xs ^ A[r + 1] == cs + A[r + 1]:
+        r += 1
+        xs ^= A[r]
+        cs += A[r]
+    result += r - l + 1
+    xs ^= A[l]
+    cs -= A[l]
+print(result)
