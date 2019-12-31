43536f2fb15ea8e7df6c2bb6165e230770a95ef0
c-yan <c-yan@users.noreply.github.com>
2019-12-31 19:12:51 +0900

    abc149d K個前に勝ったかどうかが重要なのであって、手を全部残す必要がないことに気づいたので修正

--- abc149/abc149d.py
+++ abc149/abc149d.py
@@ -2,17 +2,13 @@ N, K = map(int, input().split())
 R, S, P = map(int, input().split())
 T = input()
 
-hands = [None] * N
-d1 = {'r': 'p', 's': 'r', 'p': 's'}
-d2 = {'r': P, 's': R, 'p': S}
+d = {'r': P, 's': R, 'p': S}
 result = 0
+wins = [False] * K
 for i in range(len(T)):
-    if i >= K and d1[T[i]] == hands[i - K]:
-        if i + K < N:
-            hands[i] = list(set('rps') - set(d1[T[i]]) - set(d1[T[i + K]]))[0]
-        else:
-            hands[i] = T[i]
+    if not wins[i % K] or T[i] != T[i - K]:
+        result += d[T[i]]
+        wins[i % K] = True
     else:
-        hands[i] = d1[T[i]]
-        result += d2[T[i]]
+        wins[i % K] = False
 print(result)
