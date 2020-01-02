2fc7996d1321ac6e67b922df9b277b5ae348f6e4
c-yan <c-yan@users.noreply.github.com>
2020-01-03 00:18:23 +0900

    abc050c

---
+++ abc050/abc050c.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+if N % 2 == 0:
+    B = list(range(1, N, 2)) * 2
+else:
+    B = [0] + list(range(2, N, 2)) * 2
+B.sort()
+
+if A != B:
+    print(0)
+else:
+    result = 1
+    for i in range(N // 2):
+        result *= 2
+        result %= 1000000007
+    print(result)
