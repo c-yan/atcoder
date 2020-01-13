7fe0f939f17cc30c4df1498245e565938a74296b
c-yan <c-yan@users.noreply.github.com>
2020-01-14 00:16:23 +0900

    abc079b

---
+++ abc079/abc079b.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+if N == 1:
+    print(1)
+else:
+    L = [0] * (N + 1)
+    L[0] = 2
+    L[1] = 1
+    for i in range(2, N + 1):
+        L[i] = L[i - 1] + L[i - 2]
+    print(L[N])
