0853fd8ec108d28800c4cd1045e688a1546a6ee0
c-yan <c-yan@users.noreply.github.com>
2020-01-21 20:39:27 +0900

    dpf ソースコードの改善

--- dp/dpf.py
+++ dp/dpf.py
@@ -1,6 +1,7 @@
 # PyPy なら通る
 s = input()
 t = input()
+
 dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
 for i in range(1, len(s) + 1):
     for j in range(1, len(t) + 1):
@@ -10,6 +11,7 @@ for i in range(1, len(s) + 1):
         if (s[i - 1] == t[j - 1]) and (dp[i - 1][j - 1] + 1 > k):
             k = dp[i - 1][j - 1] + 1
         dp[i][j] = k
+
 u = []
 while i > 0 and j > 0:
     if dp[i - 1][j] == dp[i][j]:
