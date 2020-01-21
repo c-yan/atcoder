d95749dfd16d5b9796931fa82ffae9fb83c5f739
c-yan <c-yan@users.noreply.github.com>
2020-01-21 22:47:27 +0900

    abc084d ソースコードの改善

--- abc084/abc084d.py
+++ abc084/abc084d.py
@@ -1,24 +1,22 @@
 # エラトステネスの篩, 累積和
 from math import sqrt
 
-N = 10 ** 5
+max_lr = 10 ** 5
 
-p = [True] * (N + 1)
-p[0] = False
-p[1] = False
-n = int(sqrt(N) + 1)
-for j in range(4, N + 1, 2):
-    p[j] = False
-for i in range(3, n + 1, 2):
-    if p[i]:
-        for j in range(i + i, N + 1, i):
-            p[j] = False
+sieve = [True] * (max_lr + 1)
+sieve[0] = False
+sieve[1] = False
+for i in range(2, int(sqrt(max_lr)) + 1):
+    if not sieve[i]:
+        continue
+    for j in range(i * i, max_lr + 1, i):
+        sieve[j] = False
 
-cs = [0] * (N + 1)
-for i in range(3, N + 1, 2):
-    if p[i] and p[(i + 1) // 2]:
+cs = [0] * (max_lr + 1)
+for i in range(3, max_lr + 1, 2):
+    if sieve[i] and sieve[(i + 1) // 2]:
         cs[i] = 1
-for i in range(1, N + 1):
+for i in range(1, max_lr + 1):
     cs[i] += cs[i - 1]
 
 Q = int(input())
