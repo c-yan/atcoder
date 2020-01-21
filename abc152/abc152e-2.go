a783ba3c115c953a3cdca1d6868c6600657a155b
c-yan <c-yan@users.noreply.github.com>
2020-01-21 23:44:41 +0900

    abc152e Go言語の多倍長整数版を追加

---
+++ abc152/abc152e-2.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+	"strconv"
+)
+
+func main() {
+	N := readInt()
+	A := make([]*big.Int, N)
+	for i := 0; i < N; i++ {
+		A[i] = big.NewInt(int64(readInt()))
+	}
+
+	t := new(big.Int)
+
+	lcm := big.NewInt(1)
+	for i := 0; i < N; i++ {
+		t.GCD(nil, nil, lcm, A[i])
+		lcm.Mul(lcm, A[i])
+		lcm.Div(lcm, t)
+	}
+
+	result := big.NewInt(0)
+	for i := 0; i < N; i++ {
+		result.Add(result, t.Div(lcm, A[i]))
+	}
+	result.Rem(result, big.NewInt(1000000007))
+
+	fmt.Println(result)
+}
+
+const (
+	ioBufferSize = 1 * 1024 * 1024 // 1 MB
+)
+
+var stdinScanner = func() *bufio.Scanner {
+	result := bufio.NewScanner(os.Stdin)
+	result.Buffer(make([]byte, ioBufferSize), ioBufferSize)
+	result.Split(bufio.ScanWords)
+	return result
+}()
+
+func readString() string {
+	stdinScanner.Scan()
+	return stdinScanner.Text()
+}
+
+func readInt() int {
+	result, err := strconv.Atoi(readString())
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
