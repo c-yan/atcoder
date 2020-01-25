ae31478ea4d89406c9be7143f30ef81d6d5145c1
c-yan <c-yan@users.noreply.github.com>
2020-01-25 10:46:15 +0900

    abc074c

---
+++ abc074/abc074c.py
@@ -0,0 +1,33 @@
+A, B, C, D, E, F = map(int, input().split())
+
+w = set()
+for i in range(F // (100 * A) + 1):
+    for j in range(F // (100 * B) + 1):
+        a = (A * i + B * j) * 100
+        if a < F:
+            w.add(a)
+w.remove(0)
+
+s = set()
+t = E * F // 100
+for i in range(t // C + 1):
+    for j in range(t // D + 1):
+        b = C * i + D * j
+        if b < t:
+            s.add(b)
+
+best_concentration = -1
+best_a = -1
+best_b = -1
+for a in w:
+    for b in sorted(s):
+        if a + b > F:
+            break
+        if b > E * a // 100:
+            break
+        concentration = 100 * b / (a + b)
+        if concentration > best_concentration:
+            best_concentration = concentration
+            best_a = a
+            best_b = b
+print(best_a + best_b, best_b)
