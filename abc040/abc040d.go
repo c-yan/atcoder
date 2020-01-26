8d0b6b29886b886e010b7baad23bcbe5e42c8abd
c-yan <c-yan@users.noreply.github.com>
2020-01-26 23:58:31 +0900

    abc040d Go版

---
+++ abc040/abc040d.go
@@ -0,0 +1,106 @@
+// Union Find 木
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func find(parent []int, i int) int {
+	if parent[i] < 0 {
+		return i
+	}
+	parent[i] = find(parent, parent[i])
+	return parent[i]
+}
+
+func unite(parent []int, i, j int) {
+	i = find(parent, i)
+	j = find(parent, j)
+	if i == j {
+		return
+	}
+	parent[j] += parent[i]
+	parent[i] = j
+}
+
+type aby struct{ a, b, y int }
+type byY []aby
+
+func (a byY) Len() int           { return len(a) }
+func (a byY) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byY) Less(i, j int) bool { return a[i].y > a[j].y }
+
+type wvi struct{ w, v, i int }
+type byW []wvi
+
+func (a byW) Len() int           { return len(a) }
+func (a byW) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byW) Less(i, j int) bool { return a[i].w > a[j].w }
+
+func main() {
+	N := readInt()
+	M := readInt()
+	roads := make([]aby, M)
+	for i := 0; i < M; i++ {
+		roads[i].a = readInt() - 1
+		roads[i].b = readInt() - 1
+		roads[i].y = readInt()
+	}
+	Q := readInt()
+	citizen := make([]wvi, Q)
+	for i := 0; i < Q; i++ {
+		citizen[i].v = readInt() - 1
+		citizen[i].w = readInt()
+		citizen[i].i = i
+	}
+
+	parent := make([]int, N)
+	for i := 0; i < N; i++ {
+		parent[i] = -1
+	}
+
+	sort.Sort(byY(roads))
+	sort.Sort(byW(citizen))
+
+	result := make([]int, Q)
+	t := 0
+	for i := 0; i < Q; i++ {
+		c := citizen[i]
+		for t < len(roads) && roads[t].y > c.w {
+			unite(parent, find(parent, roads[t].a), find(parent, roads[t].b))
+			t++
+		}
+		result[c.i] = -parent[find(parent, c.v)]
+	}
+	for i := 0; i < Q; i++ {
+		fmt.Println(result[i])
+	}
+}
+
+const (
+	ioBufferSize = 1 * 1024 * 1024 // 1 MB
+)
+
+var stdinScanner = func() *bufio.Scanner {
+	result := bufio.NewScanner(os.Stdin)
+	result.Buffer(make([]byte, ioBufferSize), ioBufferSize)
+	result.Split(bufio.ScanWords)
+	return result
+}()
+
+func readString() string {
+	stdinScanner.Scan()
+	return stdinScanner.Text()
+}
+
+func readInt() int {
+	result, err := strconv.Atoi(readString())
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
