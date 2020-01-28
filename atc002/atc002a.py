51acf854aab062fdb7705997fbdc08c07e0b8fd3
c-yan <c-yan@users.noreply.github.com>
2020-01-28 20:19:13 +0900

    atc002a ソースコードの見直し

--- atc002/atc002a.py
+++ atc002/atc002a.py
@@ -1,29 +1,27 @@
-from sys import exit, stdin
+# 幅優先探索
+R, C = map(int, input().split())
+sy, sx = map(int, input().split())
+gy, gx = map(int, input().split())
+c = [input() for _ in range(R)]
 
-r, c = map(int, stdin.readline().split())
-sy, sx = map(int, stdin.readline().split())
-sy, sx = sy - 1, sx - 1
-gy, gx = map(int, stdin.readline().split())
-gy, gx = gy - 1, gx - 1
-maze = [stdin.readline()[:-1] for _ in range(r)]
-
-distance = [[-1] * c for _ in range(r)]
-distance[sy][sx] = 0
+sy, sx, gy, gx = sy - 1, sx - 1, gy - 1, gx - 1
+moves = [[-1] * C for _ in range(R)]
+moves[sy][sx] = 0
 q = [(sy, sx)]
 while True:
     y, x = q.pop(0)
     if y == gy and x == gx:
-        print(distance[y][x])
-        exit()
-    if maze[y - 1][x] != '#' and distance[y - 1][x] == -1:
-        distance[y - 1][x] = distance[y][x] + 1
+        print(moves[y][x])
+        break
+    if c[y - 1][x] != '#' and moves[y - 1][x] == -1:
+        moves[y - 1][x] = moves[y][x] + 1
         q.append((y - 1, x))
-    if maze[y + 1][x] != '#' and distance[y + 1][x] == -1:
-        distance[y + 1][x] = distance[y][x] + 1
+    if c[y + 1][x] != '#' and moves[y + 1][x] == -1:
+        moves[y + 1][x] = moves[y][x] + 1
         q.append((y + 1, x))
-    if maze[y][x - 1] != '#' and distance[y][x - 1] == -1:
-        distance[y][x - 1] = distance[y][x] + 1
+    if c[y][x - 1] != '#' and moves[y][x - 1] == -1:
+        moves[y][x - 1] = moves[y][x] + 1
         q.append((y, x - 1))
-    if maze[y][x + 1] != '#' and distance[y][x + 1] == -1:
-        distance[y][x + 1] = distance[y][x] + 1
+    if c[y][x + 1] != '#' and moves[y][x + 1] == -1:
+        moves[y][x + 1] = moves[y][x] + 1
         q.append((y, x + 1))
