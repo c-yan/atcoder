aece014561cd145849356f73ce7dd486d9e148c4
c-yan <c-yan@users.noreply.github.com>
2020-01-28 20:09:55 +0900

    atc001b ソースコードの見直し

--- atc001/atc001b.py
+++ atc001/atc001b.py
@@ -1,38 +1,28 @@
 # Union Find 木
-from sys import stdin
+def find(parent, i):
+    t = parent[i]
+    if t == -1:
+        return i
+    t = find(parent, t)
+    parent[i] = t
+    return t
 
+def unite(parent, i, j):
+    i = find(parent, i)
+    j = find(parent, j)
+    if i == j:
+        return
+    parent[i] = j
 
-def main():
-    def find(parent, i):
-        t = parent[i]
-        if t == -1:
-            return i
-        t = find(parent, t)
-        parent[i] = t
-        return t
+N, Q = map(int, input().split())
+parent = [-1] * N
 
-    def unite(parent, i, j):
-        i = find(parent, i)
-        j = find(parent, j)
-        if i == j:
-            return
-        parent[i] = j
-
-    from builtins import int, map, range
-    readline = stdin.readline
-
-    n, q = map(int, readline().split())
-    parent = [-1] * n
-
-    for _ in range(q):
-        p, a, b = map(int, readline().split())
-        if p == 0:
-            unite(parent, a, b)
+for _ in range(Q):
+    P, A, B = map(int, input().split())
+    if P == 0:
+        unite(parent, A, B)
+    else:
+        if find(parent, A) == find(parent, B):
+            print('Yes')
         else:
-            if find(parent, a) == find(parent, b):
-                print('Yes')
-            else:
-                print('No')
-
-
-main()
+            print('No')
