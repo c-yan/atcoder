48f13c4271c42b6f09c3fa7a2da27bb323ec22ea
c-yan <c-yan@users.noreply.github.com>
2020-01-28 20:03:16 +0900

    atc001a ソースコードの見直し

--- atc001/atc001a.py
+++ atc001/atc001a.py
@@ -1,30 +1,31 @@
-from sys import exit, stdin
-from collections import deque
+# 深さ優先探索
+from sys import exit
 
-h, w = map(int, stdin.readline().split())
-q = None
-data = [None] * h
-for i in range(h):
-    data[i] = stdin.readline()[:-1]
-    if q is None:
-        t = data[i].find('s')
-        if t != -1:
-            q = deque([(i, t)])
+H, W = map(int, input().split())
+c = [input() for _ in range(H)]
+for i in range(H):
+    t = c[i].find('s')
+    if t != -1:
+        q = [(i, t)]
+        break
 
-never = [[True] * w for _ in range(h)]
-while len(q) != 0:
-    i, j = q.popleft()
-    if data[i][j] == 'g':
+queued = [[False] * W for _ in range(H)]
+while q:
+    i, j = q.pop()
+    if c[i][j] == 'g':
         print('Yes')
         exit()
-    never[i][j] = False
-    if i > 0 and data[i - 1][j] != '#' and never[i - 1][j]:
+    if i > 0 and c[i - 1][j] != '#' and not queued[i - 1][j]:
         q.append((i - 1, j))
-    if i < h - 1 and data[i + 1][j] != '#' and never[i + 1][j]:
+        queued[i - 1][j] = True
+    if i < H - 1 and c[i + 1][j] != '#' and not queued[i + 1][j]:
         q.append((i + 1, j))
-    if j > 0 and data[i][j - 1] != '#' and never[i][j - 1]:
+        queued[i + 1][j] = True
+    if j > 0 and c[i][j - 1] != '#' and not queued[i][j - 1]:
         q.append((i, j - 1))
-    if j < w - 1 and data[i][j + 1] != '#' and never[i][j + 1]:
+        queued[i][j - 1] = True
+    if j < W - 1 and c[i][j + 1] != '#' and not queued[i][j + 1]:
         q.append((i, j + 1))
+        queued[i][j + 1] = True
 
 print('No')
