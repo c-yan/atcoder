b287b6072f4d773971cc190715355720225d0123
c-yan <c-yan@users.noreply.github.com>
2020-01-31 21:38:37 +0900

    abc081b 問題文に合わせた変数名の大文字化の際にバグらせていたので修正

--- abc081/abc081b.py
+++ abc081/abc081b.py
@@ -1,10 +1,10 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-i = 0
+result = 0
 while True:
     if any(e % 2 == 1 for e in A):
         break
-    i += 1
-    a = [e // 2 for e in A]
-print(i)
+    result += 1
+    A = [e // 2 for e in A]
+print(result)
