78d10364da1e99238325b0c03666b02686d79a77
c-yan <c-yan@users.noreply.github.com>
2020-02-14 23:11:32 +0900

    abc154d 別解(累積和)

---
+++ abc154/abc154d-2.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+p = list(map(int, input().split()))
+
+m = [(e + 1) / 2 for e in p]
+for i in range(1, N):
+    m[i] += m[i - 1]
+
+result = 0
+for i in range(N - K):
+    t = m[K - 1 + i] - m[i]
+    if t > result:
+        result = t
+print(result)
