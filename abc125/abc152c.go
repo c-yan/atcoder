f73803b1ce2172588b9a508fc6ad230d52404c5e
c-yan <c-yan@users.noreply.github.com>
2020-02-14 21:07:36 +0900

    abc125c Go言語版

---
+++ abc125/abc152c.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func gcd(a, b int) int {
+	if a < b {
+		a, b = b, a
+	}
+	for b > 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+type segmentTree struct {
+	data   []int
+	offset int
+}
+
+func newSegmentTree(n int) segmentTree {
+	var result segmentTree
+	t := 1
+	for t < n {
+		t *= 2
+	}
+	result.offset = t - 1
+	result.data = make([]int, 2*t-1)
+	return result
+}
+
+func (st segmentTree) updateAll(a []int) {
+	for i, v := range a {
+		st.data[st.offset+i] = v
+	}
+	for i := st.offset - 1; i > -1; i-- {
+		st.data[i] = gcd(st.data[i*2+1], st.data[i*2+2])
+	}
+}
+
+func (st segmentTree) update(index, value int) {
+	i := st.offset + index
+	st.data[i] = value
+	for i >= 1 {
+		i = (i - 1) / 2
+		st.data[i] = gcd(st.data[i*2+1], st.data[i*2+2])
+	}
+}
+
+func (st segmentTree) query(start, stop int) int {
+	result := 0
+	l := start + st.offset
+	r := stop + st.offset
+	for l < r {
+		if l&1 == 0 {
+			result = gcd(result, st.data[l])
+		}
+		if r&1 == 0 {
+			result = gcd(result, st.data[r-1])
+		}
+		l = l / 2
+		r = (r - 1) / 2
+	}
+	return result
+}
+
+func main() {
+	N := readInt()
+	A := readInts(N)
+
+	st := newSegmentTree(N)
+	st.updateAll(A)
+
+	result := st.query(1, N)
+	for i := 1; i < N-1; i++ {
+		result = max(result, gcd(st.query(0, i), st.query(i+1, N)))
+	}
+	result = max(result, st.query(0, N-1))
+
+	fmt.Println(result)
+}
+
+const (
+	ioBufferSize = 1 * 1024 * 1024 // 1 MB
+)
+
+var stdinScanner = func() *bufio.Scanner {
+	result := bufio.NewScanner(os.Stdin)
+	result.Buffer(make([]byte, ioBufferSize), ioBufferSize)
+	result.Split(bufio.ScanWords)
+	return result
+}()
+
+func readString() string {
+	stdinScanner.Scan()
+	return stdinScanner.Text()
+}
+
+func readInt() int {
+	result, err := strconv.Atoi(readString())
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func readInts(n int) []int {
+	result := make([]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = readInt()
+	}
+	return result
+}
