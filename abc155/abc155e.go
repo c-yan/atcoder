5eee8fdd074f95900014e5f0711c8f9057ff19a9
c-yan <c-yan@users.noreply.github.com>
2020-02-19 01:07:12 +0900

    abc155e Go言語版

---
+++ abc155/abc155e.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	N := readString()
+
+	b := []byte(N)
+	x, y := 0, 0
+
+	t := int(b[len(b)-1] - '0')
+	x, y = x+t, y+(10-t)
+
+	for i := len(b) - 2; i >= 0; i-- {
+		t := int(b[i] - '0')
+		x, y = min(x+t, y+(t+1)), min(x+(10-t), y+(10-(t+1)))
+	}
+	fmt.Println(min(x, y+1))
+}
+
+const (
+	ioBufferSize = 1 * 1024 * 1024 // 1 MB
+)
+
+var stdinScanner = func() *bufio.Scanner {
+	result := bufio.NewScanner(os.Stdin)
+	result.Buffer(make([]byte, ioBufferSize), ioBufferSize)
+	result.Split(bufio.ScanWords)
+	return result
+}()
+
+func readString() string {
+	stdinScanner.Scan()
+	return stdinScanner.Text()
+}
