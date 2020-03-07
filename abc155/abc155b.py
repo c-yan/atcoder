574d91ef1e4b9ac9a8467ad78a8fbe46e0c386d7
c-yan <c-yan@users.noreply.github.com>
2020-03-08 01:44:29 +0900

    abc155b コードの変更

--- abc155/abc155b.py
+++ abc155/abc155b.py
@@ -1,11 +1,12 @@
 N = int(input())
 A = list(map(int, input().split()))
 
+result = 'APPROVED'
 for a in A:
     if a % 2 == 1:
         continue
     if a % 3 == 0 or a % 5 == 0:
         continue
-    print('DENIED')
-    exit()
-print('APPROVED')
+    result = 'DENIED'
+    break
+print(result)
