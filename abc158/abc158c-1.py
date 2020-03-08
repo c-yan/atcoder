f0407b46f2a89227cbe3741095cb68566fa403ef
c-yan <c-yan@users.noreply.github.com>
2020-03-08 19:48:02 +0900

    abc158c コードの変更

--- abc158/abc158c-1.py
+++ abc158/abc158c-1.py
@@ -1,7 +1,8 @@
 A, B = map(int, input().split())
 
+result = -1
 for i in range(2000):
     if int(i * 0.08) == A and int(i * 0.1) == B:
-        print(i)
-        exit()
-print(-1)
+        result = i
+        break
+print(result)
