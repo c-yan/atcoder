ba4f1440065e906179f67c9f89cb9982e4725ff7
c-yan <c-yan@users.noreply.github.com>
2020-03-11 21:14:34 +0900

    abc046d ソースコードの改善

--- abc046/abc046d.py
+++ abc046/abc046d.py
@@ -1,20 +1,3 @@
 s = input()
 
-g = s.count('g')
-p = s.count('p')
-
-t = list(s)
-for i in range(len(s) - 1, -1, -1):
-    if t[i] == 'g' and g > p + 1:
-        t[i] = 'p'
-        g -= 1
-        p += 1
-t = ''.join(t)
-
-result = 0
-for i in range(len(s)):
-    if t[i] == 'p' and s[i] == 'g':
-        result += 1
-    elif t[i] == 'g' and s[i] == 'p':
-        result -= 1
-print(result)
+print(max((s.count('g') * 2 - len(s)) // 2, 0))
