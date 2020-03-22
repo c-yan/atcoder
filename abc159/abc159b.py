a26a7a289df76792303bab26b9ee6098f6fcbf67
c-yan <c-yan@users.noreply.github.com>
2020-03-22 23:19:33 +0900

    abc159b コードの改善

--- abc159/abc159b.py
+++ abc159/abc159b.py
@@ -2,10 +2,7 @@ S = input()
 
 
 def is_palindrome(s):
-    for i in range(len(s) // 2):
-        if s[i] != s[len(s) - 1 - i]:
-            return False
-    return True
+    return s == s[::-1]
 
 
 N = len(S)
