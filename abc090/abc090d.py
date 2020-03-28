4cc3ee7d218257ebd3f1cb54e0f8cd157baf9308
c-yan <c-yan@users.noreply.github.com>
2020-03-29 03:46:37 +0900

    abc090d ソースコードの改善

--- abc090/abc090d.py
+++ abc090/abc090d.py
@@ -2,5 +2,5 @@ N, K = map(int, input().split())
 
 result = 0
 for b in range(K + 1, N + 1):
-    result += (N // b) * (b - K) + max(N - (N // b) * b - max(K - 1, 0), 0)
+    result += (N // b) * (b - K) + max(N % b - max(K - 1, 0), 0)
 print(result)
