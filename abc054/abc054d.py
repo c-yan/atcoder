b08640b8fefed260fd9b9732cf20e9464e63068e
c-yan <c-yan@users.noreply.github.com>
2020-03-29 14:07:46 +0900

    abc054d, abc133d コードの改善

--- abc054/abc054d.py
+++ abc054/abc054d.py
@@ -1,9 +1,11 @@
 # DP
 INF = float('inf')
-n, ma, mb = map(int, input().split())
-cmax = n * 10
+
+N, Ma, Mb = map(int, input().split())
+
+cmax = N * 10
 t = [[INF] * (cmax + 1) for _ in range(cmax + 1)]
-for _ in range(n):
+for _ in range(N):
     a, b, c = map(int, input().split())
     for aa in range(cmax, 0, -1):
         for bb in range(cmax, 0, -1):
@@ -11,10 +13,11 @@ for _ in range(n):
                 continue
             t[aa + a][bb + b] = min(t[aa + a][bb + b], t[aa][bb] + c)
     t[a][b] = min(t[a][b], c)
+
 result = INF
 for a in range(1, cmax):
     for b in range(1, cmax):
-        if a * mb == b * ma:
+        if a * Mb == b * Ma:
             result = min(result, t[a][b])
 if result == INF:
     result = -1
--- abc133/abc133d.py
+++ abc133/abc133d.py
@@ -1,8 +1,8 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-t = [0] * N
-t[0] = sum(A[::2]) - sum(A[1::2])
+result = [0] * N
+result[0] = sum(A[::2]) - sum(A[1::2])
 for i in range(1, N):
-    t[i] = 2 * A[i - 1] - t[i - 1]
-print(*list(map(str, t)))
+    result[i] = 2 * A[i - 1] - result[i - 1]
+print(*result)
