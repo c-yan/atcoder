44f924d852dff51700d8a4dcbb2be7d31d5fcc92
c-yan <c-yan@users.noreply.github.com>
2020-03-31 01:51:38 +0900

    abc160e Python解

---
+++ abc160/abc160e.py
@@ -0,0 +1,13 @@
+X, Y, A, B, C = map(int, input().split())
+p = list(map(int, input().split()))
+q = list(map(int, input().split()))
+r = list(map(int, input().split()))
+
+p.sort(reverse=True)
+q.sort(reverse=True)
+r.sort(reverse=True)
+
+s = p[:X] + q[:Y] + r
+s.sort(reverse=True)
+
+print(sum(s[:X+Y]))
