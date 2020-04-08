41d41d84dc1ec8c59673728230329acf68a8acd3
c-yan <c-yan@users.noreply.github.com>
2020-04-09 02:01:23 +0900

    past201912k

---
+++ past201912-open/past201912k.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	N := readInt()
+
+	root := -1
+	children := make([][]int, N+1)
+	parent := make([][]int, 18)
+	for i := 0; i < 18; i++ {
+		parent[i] = make([]int, N+1)
+	}
+
+	for i := 1; i <= N; i++ {
+		p := readInt()
+		parent[0][i] = p
+		if p == -1 {
+			root = i
+		} else {
+			children[p] = append(children[p], i)
+		}
+	}
+
+	for i := 1; i < 18; i++ {
+		for j := 1; j <= N; j++ {
+			t := parent[i-1][j]
+			if t == -1 {
+				parent[i][j] = -1
+			} else {
+				parent[i][j] = parent[i-1][t]
+			}
+		}
+	}
+
+	depth := make([]int, N+1)
+	q := [][2]int{[2]int{root, 0}}
+	for len(q) != 0 {
+		e := q[0]
+		q = q[1:]
+		n, d := e[0], e[1]
+		depth[n] = d
+		for _, c := range children[n] {
+			q = append(q, [2]int{c, d + 1})
+		}
+	}
+
+	Q := readInt()
+	for i := 0; i < Q; i++ {
+		a := readInt()
+		b := readInt()
+		if depth[a] <= depth[b] {
+			fmt.Println("No")
+			continue
+		}
+		for depth[a] != depth[b] {
+			t := int(math.Log2(float64(depth[a] - depth[b])))
+			a = parent[t][a]
+		}
+		if a == b {
+			fmt.Println("Yes")
+		} else {
+			fmt.Println("No")
+		}
+	}
+}
+
+const (
+	ioBufferSize = 1 * 1024 * 1024 // 1 MB
+)
+
+var stdinScanner = func() *bufio.Scanner {
+	result := bufio.NewScanner(os.Stdin)
+	result.Buffer(make([]byte, ioBufferSize), ioBufferSize)
+	result.Split(bufio.ScanWords)
+	return result
+}()
+
+func readString() string {
+	stdinScanner.Scan()
+	return stdinScanner.Text()
+}
+
+func readInt() int {
+	result, err := strconv.Atoi(readString())
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func readInts(n int) []int {
+	result := make([]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = readInt()
+	}
+	return result
+}
