91219097ef540e2913ba0ed3f93978628380c444
c-yan <c-yan@users.noreply.github.com>
2020-04-11 00:09:45 +0900

    sumitrust2019d フォーマッタのかけ忘れを修正

--- sumitrust2019/sumitrust2019d.py
+++ sumitrust2019/sumitrust2019d.py
@@ -3,14 +3,14 @@ S = input()
 
 result = 0
 for i in range(10):
-  a = S.find(str(i))
-  if a == -1:
-    continue
-  for j in range(10):
-    b = S.find(str(j), a + 1)
-    if b == -1:
-      continue
-    for k in range(10):
-      if S.find(str(k), b + 1) != -1:
-        result += 1
+    a = S.find(str(i))
+    if a == -1:
+        continue
+    for j in range(10):
+        b = S.find(str(j), a + 1)
+        if b == -1:
+            continue
+        for k in range(10):
+            if S.find(str(k), b + 1) != -1:
+                result += 1
 print(result)
