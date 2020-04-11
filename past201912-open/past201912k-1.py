f6f1450a2a87427dd66a000f02beb6e4f62eb991
c-yan <c-yan@users.noreply.github.com>
2020-04-12 02:06:43 +0900

    past201912k Python版

---
+++ past201912-open/past201912k-1.py
@@ -0,0 +1,35 @@
+N = int(input())
+
+root = -1
+children = [[] for _ in range(N + 1)]
+left = [0] * (N + 1)
+right = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    p = int(input())
+    if p == -1:
+        root = i
+    else:
+        children[p].append(i)
+
+i = 0
+s = [root]
+while s:
+    n = s.pop()
+    if n > 0:
+        left[n] = i
+        i += 1
+        s.append(-n)
+        for c in children[n]:
+            s.append(c)
+    else:
+        right[-n] = i
+        i += 1
+
+Q = int(input())
+for _ in range(Q):
+    a, b = map(int, input().split())
+    if left[b] <= left[a] <= right[b]:
+        print('Yes')
+    else:
+        print('No')
---
+++ past201912-open/past201912k-2.py
@@ -0,0 +1,36 @@
+from sys import setrecursionlimit
+
+
+def euler_tour(n, i, left, right):
+    left[n] = i
+    for c in children[n]:
+        i = euler_tour(c, i + 1, left, right)
+    right[n] = i
+    return i
+
+
+setrecursionlimit(200000)
+
+N = int(input())
+
+root = -1
+children = [[] for _ in range(N + 1)]
+left = [0] * (N + 1)
+right = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    p = int(input())
+    if p == -1:
+        root = i
+    else:
+        children[p].append(i)
+
+euler_tour(root, 0, left, right)
+
+Q = int(input())
+for _ in range(Q):
+    a, b = map(int, input().split())
+    if left[b] <= left[a] <= right[b]:
+        print('Yes')
+    else:
+        print('No')
