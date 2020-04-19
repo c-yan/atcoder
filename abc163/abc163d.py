2b7788abafdc12f4b2dc4a530d2ce282c8777a95
c-yan <c-yan@users.noreply.github.com>
2020-04-20 00:04:33 +0900

    abc163d

---
+++ abc163/abc163d.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+
+result = 0
+for i in range(K, N + 2):
+    # max: N, N -1, ..., N - i + 1
+    a = (N + (N - i + 1)) * i // 2
+    # min: 0, 1, .., i - 1
+    b = (0 + (i - 1)) * i // 2
+    result += a - b + 1
+    result %= 1000000007
+print(result)
