6b52355f2c8f188d407356c85e1cb7e17dac154e
c-yan <c-yan@users.noreply.github.com>
2020-05-03 13:09:57 +0900

    abc165c, abc165d ソースコードの改善

--- abc165/abc165c.py
+++ abc165/abc165c.py
@@ -1,26 +1,10 @@
+from itertools import combinations_with_replacement
+
 N, M, Q = map(int, input().split())
 abcd = [list(map(int, input().split())) for _ in range(Q)]
 
-m = M - 1
-
-
-def f(s):
-    if len(s) == N:
-        return [s]
-    result = []
-    if s == '':
-        r = 0
-    else:
-        r = int(s[-1])
-    for i in range(r, m + 1):
-        result.extend(f(s + str(i)))
-    return result
-
-
 result = 0
-V = f('')
-for s in V:
-    A = [int(c) + 1 for c in s]
+for A in combinations_with_replacement(range(1, M + 1), N):
     t = 0
     for a, b, c, d in abcd:
         if A[b - 1] - A[a - 1] == c:
--- abc165/abc165d.py
+++ abc165/abc165d.py
@@ -2,20 +2,5 @@ from math import floor
 
 A, B, N = map(int, input().split())
 
-
-def f(x):
-    return floor(A * x / B) - A * floor(x / B)
-
-
-result = 0
-for x in range(min(A + 1, N + 1)):
-    result = max(result, f(x))
-
-if B > A:
-    for i in range(10 ** 20):
-        x = (i * B + A - 1) // A
-        if x > N:
-            break
-        result = max(result, f(x))
-
-print(result)
+x = min(B - 1, N)
+print(floor(A * x / B) - A * floor(x / B))
