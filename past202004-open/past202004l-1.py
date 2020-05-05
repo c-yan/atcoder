7206b448980f3a40d5b7def188bd0c12c54698cc
c-yan <c-yan@users.noreply.github.com>
2020-05-05 17:54:31 +0900

    past202004l 高速化

--- past202004-open/past202004l-1.py
+++ past202004-open/past202004l-1.py
@@ -12,26 +12,29 @@ class SegmentTree():
         self._data = [0] * (t * 2 - 1)
 
     def update_all(self, iterable):
-        self._data[self._offset:self._offset+self._size] = iterable
+        data = self._data
+        data[self._offset:self._offset + self._size] = iterable
         for i in range(self._offset - 1, -1, -1):
-            self._data[i] = min(self._data[i * 2 + 1], self._data[i * 2 + 2])
+            data[i] = min(data[i * 2 + 1], data[i * 2 + 2])
 
     def update(self, index, value):
+        data = self._data
         i = self._offset + index
-        self._data[i] = value
+        data[i] = value
         while i >= 1:
             i = (i - 1) // 2
-            self._data[i] = min(self._data[i * 2 + 1], self._data[i * 2 + 2])
+            data[i] = min(data[i * 2 + 1], data[i * 2 + 2])
 
     def query(self, start, stop):
+        data = self._data
         result = float('inf')
         l = start + self._offset
         r = stop + self._offset
         while l < r:
             if l & 1 == 0:
-                result = min(result, self._data[l])
+                result = min(result, data[l])
             if r & 1 == 0:
-                result = min(result, self._data[r - 1])
+                result = min(result, data[r - 1])
             l = l // 2
             r = (r - 1) // 2
         return result
