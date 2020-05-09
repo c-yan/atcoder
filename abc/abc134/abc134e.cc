a3c5f4e3beee7e882914aa7dffb40662e1142b47
c-yan <c-yan@users.noreply.github.com>
2020-05-09 21:50:31 +0900

    abc134e C++別解(平衡二分探索木)

---
+++ abc/abc134/abc134e.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ll N;
+	cin >> N;
+	vector<ll> A(N);
+	rep(i, N) cin >> A[i];
+
+	multiset<ll> t;
+	for (auto a : A) {
+		auto it = t.lower_bound(a);
+		if (it != t.begin()) {
+			t.erase(--it);
+		}
+		t.insert(a);
+	}
+	cout << t.size() << endl;
+
+	return 0;
+}
