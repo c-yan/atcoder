fc58813283b9e4f932f58f04dc53c973f3f14a2c
c-yan <c-yan@users.noreply.github.com>
2020-05-11 08:39:27 +0900

    abc167c 別解

---
+++ abc/abc167/abc167c-2.py
@@ -0,0 +1,27 @@
+from itertools import product
+
+N, M, X = map(int, input().split())
+
+C = []
+A = []
+for i in range(N):
+    t = list(map(int, input().split()))
+    C.append(t[0])
+    A.append(t[1:])
+
+result = -1
+for p in product([True, False], repeat = N):
+    t = [0] * M
+    c = 0
+    for j in range(N):
+        if not p[j]:
+            continue
+        c += C[j]
+        for k in range(M):
+            t[k] += A[j][k]
+    if all(x >= X for x in t):
+        if result == -1:
+            result = c
+        else:
+            result = min(result, c)
+print(result)
