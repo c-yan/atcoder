ff3b5fe9d931f0610803784ece59fd872b1c5b26
c-yan <c-yan@users.noreply.github.com>
2020-05-23 12:09:37 +0900

    abc071d

---
+++ abc/abc071/abc071d.py
@@ -0,0 +1,25 @@
+N = int(input())
+S1 = input()
+S2 = input()
+
+if S1[0] == S2[0]:
+    result = 3
+else:
+    result = 6
+x = 0
+W = len(S1)
+while x < W:
+    if S1[x] == S2[x]:
+        if x + 1 == W:
+            break
+        result *= 2
+        result %= 1000000007
+        x += 1
+    else:
+        if x + 2 == W:
+            break
+        if S1[x + 2] != S2[x + 2]:
+            result *= 3
+            result %= 1000000007
+        x += 2
+print(result)
