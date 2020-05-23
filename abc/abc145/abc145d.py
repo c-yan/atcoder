af9250979ce212da0999e7a8c98c590f65a4f10d
c-yan <c-yan@users.noreply.github.com>
2020-05-24 01:09:05 +0900

    abc145d ソースコードの改善

--- abc/abc145/abc145d.py
+++ abc/abc145/abc145d.py
@@ -1,22 +1,9 @@
 # フェルマーの小定理
-def mcomb(n, k):
-    if n == 0 and k == 0:
-        return 1
-    if n < k or k < 0:
-        return 0
-
-    fac = [0] * (n + 1)
-    fac[0] = 1
-    for i in range(n):
-        fac[i + 1] = fac[i] * (i + 1) % p
-    return fac[n] * pow(fac[n - k], p - 2, p) * pow(fac[k], p - 2, p) % p
-
-
 p = 1000000007
 
 X, Y = map(int, input().split())
 
-if (X+Y) % 3 != 0:
+if (X + Y) % 3 != 0:
     print(0)
     exit()
 
@@ -27,4 +14,19 @@ if a < 0 or b < 0:
     print(0)
     exit()
 
-print(mcomb(a + b, min(a, b)))
+n = a + b
+fac = [0] * (n + 1)
+fac[0] = 1
+for i in range(n):
+    fac[i + 1] = fac[i] * (i + 1) % p
+
+
+def mcomb(n, k):
+    if n == 0 and k == 0:
+        return 1
+    if n < k or k < 0:
+        return 0
+    return fac[n] * pow(fac[n - k], p - 2, p) * pow(fac[k], p - 2, p) % p
+
+
+print(mcomb(n, a))
