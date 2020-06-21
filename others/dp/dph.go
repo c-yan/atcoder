eb471bfc895dd1cc8e6719409c83a67894c0c17f
c-yan <c-yan@users.noreply.github.com>
2020-06-21 09:16:50 +0900

    dph ソースコードの改善

--- others/dp/dph.go
+++ others/dp/dph.go
@@ -12,17 +12,19 @@ const (
 )
 
 func main() {
-	h := readInt()
-	w := readInt()
+	H := readInt()
+	W := readInt()
 	a := make([]string, 2)
 	a[0] = readString()
 	a[1] = readString()
+
 	dp := make([][]int, 2)
-	dp[0] = make([]int, w)
-	dp[1] = make([]int, w)
+	dp[0] = make([]int, W)
+	dp[1] = make([]int, W)
 	dp[0][0] = 1
-	for i := 0; i < h-1; i++ {
-		for j := 0; j < w-1; j++ {
+
+	for i := 0; i < H-1; i++ {
+		for j := 0; j < W-1; j++ {
 			if a[0][j+1] != '#' {
 				dp[0][j+1] = (dp[0][j+1] + dp[0][j]) % m
 			}
@@ -30,21 +32,23 @@ func main() {
 				dp[1][j] = dp[0][j]
 			}
 		}
-		if a[1][w-1] != '#' {
-			dp[1][w-1] = dp[0][w-1]
+		if a[1][W-1] != '#' {
+			dp[1][W-1] = dp[0][W-1]
 		}
+
 		a[0], a[1] = a[1], readString()
 		dp[0], dp[1] = dp[1], dp[0]
-		for j := 0; j < w; j++ {
+		for j := 0; j < W; j++ {
 			dp[1][j] = 0
 		}
 	}
-	for j := 0; j < w-1; j++ {
+	for j := 0; j < W-1; j++ {
 		if a[0][j+1] != '#' {
 			dp[0][j+1] = (dp[0][j+1] + dp[0][j]) % m
 		}
 	}
-	fmt.Println(dp[0][w-1])
+
+	fmt.Println(dp[0][W-1])
 }
 
 const (
@@ -69,14 +73,4 @@ func readInt() int {
 		panic(err)
 	}
 	return result
-}
-
-func printIntln(v ...int) {
-	b := make([]byte, 0, 4096)
-	for i := 0; i < len(v)-1; i++ {
-		b = append(b, strconv.Itoa(v[i])...)
-		b = append(b, " "...)
-	}
-	b = append(b, strconv.Itoa(v[len(v)-1])...)
-	fmt.Println(string(b))
 }
