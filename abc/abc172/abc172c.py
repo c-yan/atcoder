f0801ccb9c772e36b3127501ccee8905ce0f9a3d
c-yan <c-yan@users.noreply.github.com>
2020-06-27 23:51:45 +0900

    abc172c ソースコードの改善

--- abc/abc172/abc172c.py
+++ abc/abc172/abc172c.py
@@ -5,26 +5,19 @@ N, M, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-a = list(accumulate(A))
-b = list(accumulate(B))
+a = [0] + list(accumulate(A))
+b = [0] + list(accumulate(B))
 
 result = 0
-j = bisect_left(b, K)
-if j == M:
-  result = M
-elif b[j] == K:
-    result = j + 1
-else:
-    result = j
-for i in range(N):
+for i in range(N + 1):
     t = K - a[i]
     if t < 0:
         break
     j = bisect_left(b, t)
-    if j == M:
-        result = max(result, i + 1 + M)
+    if j == M + 1:
+        result = max(result, i + M)
     elif b[j] == t:
-        result = max(result, i + 1 + j + 1)
+        result = max(result, i + j)
     else:
-        result = max(result, i + 1 + j)
+        result = max(result, i + j - 1)
 print(result)
