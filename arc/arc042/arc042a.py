cd042294bb69e7dd6660647df48aa66af1c27693
c-yan <c-yan@users.noreply.github.com>
2020-07-05 00:23:40 +0900

    arc042a

---
+++ arc/arc042/arc042a.py
@@ -0,0 +1,7 @@
+N, M, *a = map(int, open(0).read().split())
+
+t = list(range(N - 1, -1, -1))
+for i in range(M):
+    t[a[i] - 1] = N + i
+
+print('\n'.join(str(e[1]) for e in sorted([(t[i], i + 1) for i in range(N)], reverse=True)))
