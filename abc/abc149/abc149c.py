9c6385e5ab6cfc97cfb39a1d6b11df0d0884ddb1
c-yan <c-yan@users.noreply.github.com>
2020-07-06 02:51:19 +0900

    abc149c エラトステネスの篩を関数化

--- abc/abc149/abc149c.py
+++ abc/abc149/abc149c.py
@@ -1,22 +1,22 @@
 # エラトステネスの篩
-from math import sqrt
-
-N = 10 ** 6
-
-p = [True] * (N + 1)
-p[0] = False
-p[1] = False
-n = int(sqrt(N) + 1)
-for j in range(4, N + 1, 2):
-  p[j] = False
-for i in range(3, n + 1, 2):
-  if p[i]:
-    for j in range(i + i, N + 1, i):
-      p[j] = False
+def make_prime_table(n):
+    sieve = list(range(n + 1))
+    sieve[0] = -1
+    sieve[1] = -1
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i] != i:
+            continue
+        for j in range(i * i, n + 1, i):
+            if sieve[j] == j:
+                sieve[j] = i
+    return sieve
 
 
 X = int(input())
+
+N = 10 ** 6
+prime_table = make_prime_table(N)
 for i in range(X, N + 1):
-    if p[i]:
+    if prime_table[i] == i:
         print(i)
         break
