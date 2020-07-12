612b7246625e91fc56c953d62aa7f8ee8c393c2f
c-yan <c-yan@users.noreply.github.com>
2020-07-12 11:39:51 +0900

    aising2020c ソースコードの改善

--- others/aising2020/aising2020c.py
+++ others/aising2020/aising2020c.py
@@ -1,11 +1,12 @@
 N = int(input())
 
-t = [0] * (10 ** 4 + 1)
+result = [0] * (N + 1)
 for x in range(1, 101):
     for y in range(1, 101):
         for z in range(1, 101):
             n = x * x + y * y + z * z + x * y + y * z + z * x
-            if n <= 10 ** 4:
-                t[n] += 1
+            if n > N:
+                break
+            result[n] += 1
 
-print(*[t[i] for i in range(1, N + 1)], sep='\n')
+print(*result[1:], sep='\n')
