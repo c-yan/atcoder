a06b7d89b94b37ab2a2cbdcde9ae58419d982bda
c-yan <c-yan@users.noreply.github.com>
2020-07-27 00:09:30 +0900

    m-solutions2020d ソースコードの改善

--- others/m-solutions2020/m-solutions2020d.py
+++ others/m-solutions2020/m-solutions2020d.py
@@ -4,9 +4,8 @@ A = list(map(int, input().split()))
 t = [-1] * (N + 1)
 t[0] = 1000
 for i in range(N):
-    t[i + 1] = max(t[i + 1], t[i])
     k = t[i] // A[i]
     y = t[i] % A[i]
-    for j in range(i + 1, N):
+    for j in range(i, N):
         t[j + 1] = max(t[j + 1], k * A[j] + y)
 print(t[N])
