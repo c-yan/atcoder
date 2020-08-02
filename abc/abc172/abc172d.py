e53beed47ceefc6330011c4c08ac8c752e5ca393
c-yan <c-yan@users.noreply.github.com>
2020-08-03 05:17:57 +0900

    all エラトステネスの篩を高速化

--- abc/abc172/abc172d.py
+++ abc/abc172/abc172d.py
@@ -3,10 +3,12 @@ def make_prime_table(n):
     sieve = list(range(n + 1))
     sieve[0] = -1
     sieve[1] = -1
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(4, n + 1, 2):
+        sieve[i] = 2
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if sieve[i] != i:
             continue
-        for j in range(i * i, n + 1, i):
+        for j in range(i * i, n + 1, i * 2):
             if sieve[j] == j:
                 sieve[j] = i
     return sieve
