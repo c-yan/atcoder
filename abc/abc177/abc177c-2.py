3125ea6897652041c55827db283aa54bb964890a
c-yan <c-yan@users.noreply.github.com>
2020-09-05 08:34:40 +0900

    abc177c コードの改善

--- abc/abc177/abc177c-2.py
+++ abc/abc177/abc177c-2.py
@@ -3,9 +3,10 @@ m = 1000000007
 N, *A = map(int, open(0).read().split())
 
 result = 0
-a = 0
+c = 0
 for i in range(1, N + 1):
-    result += a * A[N - i]
-    a += A[N - i]
+    result += A[N - i] * c
     result %= m
+    c += A[N - i]
+    c %= m
 print(result)
