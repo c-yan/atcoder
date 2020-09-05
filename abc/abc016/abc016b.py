866c9fd5fadd5cd1f8d3dc2d10543f6151b705a0
c-yan <c-yan@users.noreply.github.com>
2020-09-06 01:10:20 +0900

    abc016b

---
+++ abc/abc016/abc016b.py
@@ -0,0 +1,12 @@
+A, B, C = map(int, input().split())
+
+if A + B == C:
+    if A - B == C:
+        print('?')
+    else:
+        print('+')
+else:
+    if A - B == C:
+        print('-')
+    else:
+        print('!')
