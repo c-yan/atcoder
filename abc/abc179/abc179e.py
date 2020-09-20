db848cd1c9bd0ae7e7f4cf43b7899b89703cd94d
c-yan <c-yan@users.noreply.github.com>
2020-09-20 18:03:09 +0900

    abc179e ソースコードの改善

--- abc/abc179/abc179e.py
+++ abc/abc179/abc179e.py
@@ -10,15 +10,16 @@ for i in range(N):
     a.append(A)
     A = A * A % M
 
-for i in range(len(a)):
-    if a[i] == A:
-        break
-loop_start = i
+try:
+    loop_start = a.index(A)
+except:
+    loop_start = len(a)
 
 result = sum(a[:loop_start])
-a = a[loop_start:]
 N -= loop_start
-loops = N // len(a)
-remainder = N % len(a)
-result += sum(a) * loops + sum(a[:remainder])
+if N != 0:
+    a = a[loop_start:]
+    loops = N // len(a)
+    remainder = N % len(a)
+    result += sum(a) * loops + sum(a[:remainder])
 print(result)
