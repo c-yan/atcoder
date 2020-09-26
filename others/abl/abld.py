bedd24b4e795351c5af0a4f142d3d28ebaa51620
c-yan <c-yan@users.noreply.github.com>
2020-09-27 04:05:56 +0900

    abld PyPy版

---
+++ others/abl/abld.py
@@ -0,0 +1,52 @@
+class SegmentTree:
+    def __init__(self, size, op, e):
+        self._op = op
+        self._e = e
+        self._size = size
+        t = 1
+        while t < size:
+            t *= 2
+        self._offset = t - 1
+        self._data = [e] * (t * 2 - 1)
+
+    def build(self, iterable):
+        op = self._op
+        data = self._data
+        data[self._offset:self._offset + self._size] = iterable
+        for i in range(self._offset - 1, -1, -1):
+            data[i] = op(data[i * 2 + 1], data[i * 2 + 2])
+
+    def update(self, index, value):
+        op = self._op
+        data = self._data
+        i = self._offset + index
+        data[i] = value
+        while i >= 1:
+            i = (i - 1) // 2
+            data[i] = op(data[i * 2 + 1], data[i * 2 + 2])
+
+    def query(self, start, stop):
+        def iter_segments(data, l, r):
+            while l < r:
+                if l & 1 == 0:
+                    yield data[l]
+                if r & 1 == 0:
+                    yield data[r - 1]
+                l = l // 2
+                r = (r - 1) // 2
+        op = self._op
+        it = iter_segments(self._data, start + self._offset,
+                           stop + self._offset)
+        result = self._e
+        for v in it:
+            result = op(result, v)
+        return result
+
+
+N, K, *A = map(int, open(0).read().split())
+
+st = SegmentTree(300000 + 1, max, 0)
+for a in A:
+    st.update(a, st.query(max(a - K, 0), min(a + K + 1, 300000 + 1)) + 1)
+
+print(st.query(0, 300000 + 1))
