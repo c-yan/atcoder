f7ca0734161d5dc80fb52251fc23e3522521d585
c-yan <c-yan@users.noreply.github.com>
2020-12-24 04:10:05 +0900

    abc174f ソースコードの改善

--- abc/abc174/abc174f-2.go
+++ abc/abc174/abc174f-2.go
@@ -42,7 +42,7 @@ func main() {
 
 	c := make([]int, N)
 	for i := 0; i < N; i++ {
-		c[i] = readInt()
+		c[i] = readInt() - 1
 	}
 
 	lr := make([][3]int, Q)
@@ -55,11 +55,14 @@ func main() {
 
 	result := make([]int, Q)
 	bit := newBIT(N)
-	mr := map[int]int{}
+	mr := make([]int, N)
+	for i := 0; i < N; i++ {
+		mr[i] = -1
+	}
 	k := 0
 	for i := 0; i < N; i++ {
-		if j, ok := mr[c[i]]; ok {
-			bit.add(j, -1)
+		if mr[c[i]] != -1 {
+			bit.add(mr[c[i]], -1)
 		}
 		bit.add(i, 1)
 		mr[c[i]] = i
