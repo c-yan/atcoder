99869e5a36e15e1822d8f334babf644ac965ca74
c-yan <c-yan@users.noreply.github.com>
2021-01-01 02:49:08 +0900

    abc066d

---
+++ abc/abc066/abc066d.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+def make_factorial_table(n):
+    result = [0] * (n + 1)
+    result[0] = 1
+    for i in range(1, n + 1):
+        result[i] = result[i - 1] * i % m
+    return result
+
+
+def mcomb(n, k):
+    if n == 0 and k == 0:
+        return 1
+    if n < k or k < 0:
+        return 0
+    return fac[n] * pow(fac[n - k], m - 2, m) * pow(fac[k], m - 2, m) % m
+
+m = 1000000007
+
+n, *a = map(int, open(0).read().split())
+
+b = Counter(a).most_common(1)[0][0]
+l = a.index(b)
+r = a.index(b, l + 1)
+
+fac = make_factorial_table(n + 1)
+result = []
+for k in range(1, n + 2):
+    t = mcomb(n + 1, k) - mcomb(n - (r - l),k - 1)
+    t %= m
+    result.append(t)
+print(*result, sep='\n')
