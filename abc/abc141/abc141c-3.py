a6897e4b64870ba8d9e56d2cf83edf5ebb87a038
c-yan <c-yan@users.noreply.github.com>
2021-01-03 16:02:13 +0900

    abc141c ソースコードの改善

--- abc/abc141/abc141c-3.py
+++ abc/abc141/abc141c-3.py
@@ -10,7 +10,7 @@ class RangeAddSegmentTree:
         self._offset = t - 1
         self._data = [0] * (t * 2 - 1)
 
-    def add(self, start, stop, x):
+    def range_add(self, start, stop, x):
         data = self._data
         l = start + self._offset
         r = stop + self._offset
@@ -22,27 +22,27 @@ class RangeAddSegmentTree:
             l = l // 2
             r = (r - 1) // 2
 
-    def get(self, i):
+    def __getitem__(self, key):
         data = self._data
-        j = i + self._offset
-        result = data[j]
-        while j > 0:
-            j = (j - 1) // 2
-            result += data[j]
+        i = key + self._offset
+        result = data[i]
+        while i > 0:
+            i = (i - 1) // 2
+            result += data[i]
         return result
 
 
 N, K, Q, *A = map(int, open(0).read().split())
 
 st = RangeAddSegmentTree(N)
-st.add(0, N, K)
+st.range_add(0, N, K)
 for a in A:
-    st.add(0, a - 1, -1)
-    st.add(a, N, -1)
+    st.range_add(0, a - 1, -1)
+    st.range_add(a, N, -1)
 
 result = []
 for i in range(N):
-    if st.get(i) > 0:
+    if st[i] > 0:
         result.append('Yes')
     else:
         result.append('No')
