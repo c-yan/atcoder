e346ab54b8da0da59e62bae1a255b679da0db021
c-yan <c-yan@users.noreply.github.com>
2021-01-03 18:14:49 +0900

    abc187b 別解(割り算しない版)

---
+++ abc/abc187/abc187b-2.py
@@ -0,0 +1,11 @@
+N = int(input())
+xy = [tuple(map(int, input().split())) for _ in range(N)]
+
+result = 0
+for i in range(N):
+    x1, y1 = xy[i]
+    for j in range(i + 1, N):
+        x2, y2 = xy[j]
+        if abs(y2 - y1) <= abs(x2 - x1):
+            result += 1
+print(result)
