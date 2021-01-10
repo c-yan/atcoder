e75e55c73246aec11590a3f61123858e4145fb0e
c-yan <c-yan@users.noreply.github.com>
2021-01-10 23:27:33 +0900

    abc188e ソースコードの改善

--- abc/abc188/abc188e.py
+++ abc/abc188/abc188e.py
@@ -4,19 +4,19 @@ readline = stdin.readline
 
 N, M = map(int, readline().split())
 A = list(map(int, readline().split()))
-XY = [tuple(map(lambda x: int(x) - 1, readline().split())) for _ in range(M)]
 
 links = [[] for _ in range(N)]
-for x, y in XY:
-    links[x].append(y)
+for _ in range(M):
+    X, Y = map(lambda x: int(x) - 1, readline().split())
+    links[X].append(Y)
 
 maxvs = [None] * N
 result = -(10 ** 18)
 for i in range(N - 1, -1, -1):
-    maxv = 0
-    for j in links[i]:
-        maxv = max(maxv, maxvs[j])
-    if len(links[i]) != 0:
-        result = max(result, maxv - A[i])
+    if len(links[i]) == 0:
+        maxvs[i] = A[i]
+        continue
+    maxv = max(maxvs[j] for j in links[i])
+    result = max(result, maxv - A[i])
     maxvs[i] = max(maxv, A[i])
 print(result)
