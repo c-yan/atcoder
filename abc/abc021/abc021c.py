9d795a93aaf53a34cc59a336159860aed35ff65b
c-yan <c-yan@users.noreply.github.com>
2021-01-11 22:45:20 +0900

    abc021c ソースコードの改善

--- abc/abc021/abc021c.py
+++ abc/abc021/abc021c.py
@@ -1,21 +1,6 @@
 from sys import stdin
 from collections import deque
 
-cache = {}
-def f(p):
-    if p == a:
-        return 1
-    if p in cache:
-        return cache[p]
-    d = dist[p]
-    result = 0
-    for x in links[p]:
-        if dist[x] != d - 1:
-            continue
-        result += f(x)
-    cache[p] = result
-    return result
-
 readline = stdin.readline
 m = 1000000007
 
@@ -31,8 +16,7 @@ for _ in range(M):
     links.setdefault(y, [])
     links[y].append(x)
 
-dist = {}
-dist[a] = 0
+dist = {a: 0}
 q = deque([a])
 while q:
     x = q.popleft()
@@ -42,4 +26,23 @@ while q:
         dist[y] = dist[x] + 1
         q.append(y)
 
-print(f(b) % m)
+cache = {}
+
+
+def f(p):
+    if p == a:
+        return 1
+    if p in cache:
+        return cache[p]
+    d = dist[p]
+    result = 0
+    for x in links[p]:
+        if dist[x] != d - 1:
+            continue
+        result += f(x)
+        result %= m
+    cache[p] = result
+    return result
+
+
+print(f(b))
