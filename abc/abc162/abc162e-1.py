fcf7625f4028adb8df7e605a8669383007945fe6
c-yan <c-yan@users.noreply.github.com>
2021-01-11 22:59:31 +0900

    abc162e ソースコードの改善

--- abc/abc162/abc162e-1.py
+++ abc/abc162/abc162e-1.py
@@ -1,23 +1,23 @@
+m = 1000000007
+
 N, K = map(int, input().split())
 
-MOD = 10 ** 9 + 7
-
 cache = {}
-def f(k, n):
+def f(k):
     if k == 1:
         return 1
     if k in cache:
         return cache[k]
-    result = pow(k, n, MOD)
+    result = pow(k, N, m)
     for i in range(2, k + 1):
-        result -= f(k // i, n)
-        result %= MOD
+        result -= f(k // i)
+        result %= m
     cache[k] = result
     return result
 
 
 result = 0
 for i in range(1, K + 1):
-    result += f(K // i, N) * i
-    result %= MOD
+    result += f(K // i) * i
+    result %= m
 print(result)
--- abc/abc162/abc162e-2.py
+++ abc/abc162/abc162e-2.py
@@ -1,17 +1,17 @@
-N, K = map(int, input().split())
+m = 1000000007
 
-MOD = 10 ** 9 + 7
+N, K = map(int, input().split())
 
 c = [0] * (K + 1)
 for i in range(K, 0, -1):
-    t = pow(K // i, N, MOD)
+    t = pow(K // i, N, m)
     for j in range(2, K // i + 1):
         t -= c[i * j]
-        t %= MOD
+        t %= m
     c[i] = t
 
 result = 0
 for i in range(1, K + 1):
     result += c[i] * i
-    result %= MOD
+    result %= m
 print(result)
