cbd4b0ed6e790476c487f99e1cdac398daf5a2a4
c-yan <c-yan@users.noreply.github.com>
2021-01-26 01:25:31 +0900

    abc189c 別解

---
+++ abc/abc189/abc189c-3.py
@@ -0,0 +1,70 @@
+from sys import setrecursionlimit
+
+
+class SegmentTree:
+    def __init__(self, size, op, e):
+        self._op = op
+        self._e = e
+        self._size = size
+        t = 1
+        while t < size:
+            t *= 2
+        self._offset = t - 1
+        self._data = [e] * (t * 2 - 1)
+
+    def __getitem__(self, key):
+        return self._data[self._offset + key]
+
+    def __setitem__(self, key, value):
+        op = self._op
+        data = self._data
+        i = self._offset + key
+        data[i] = value
+        while i >= 1:
+            i = (i - 1) // 2
+            data[i] = op(data[i * 2 + 1], data[i * 2 + 2])
+
+    def build(self, iterable):
+        op = self._op
+        data = self._data
+        data[self._offset:self._offset + self._size] = iterable
+        for i in range(self._offset - 1, -1, -1):
+            data[i] = op(data[i * 2 + 1], data[i * 2 + 2])
+
+    def query(self, start, stop):
+        def iter_segments(data, l, r):
+            while l < r:
+                if l & 1 == 0:
+                    yield data[l]
+                if r & 1 == 0:
+                    yield data[r - 1]
+                l = l // 2
+                r = (r - 1) // 2
+        op = self._op
+        it = iter_segments(self._data, start + self._offset,
+                           stop + self._offset)
+        result = self._e
+        for v in it:
+            result = op(result, v)
+        return result
+
+
+setrecursionlimit(10 ** 6)
+
+N, *A = map(int, open(0).read().split())
+
+st = SegmentTree(N, min, (10 ** 18, -1))
+st.build((A[i], i) for i in range(N))
+
+
+def f(l, r):
+    if l == r:
+        return 0
+    m, i = st.query(l, r)
+    result = m * (r - l)
+    result = min(result, f(l, i))
+    result = min(result, f(i + 1, r))
+    return result
+
+
+print(f(0, N))
