a2fde45b3eb7aeb547a9c6e6bac2f183168d48b5
c-yan <c-yan@users.noreply.github.com>
2021-01-30 23:41:51 +0900

    abc190f 別解

---
+++ abc/abc190/abc190f-2.py
@@ -0,0 +1,39 @@
+class BinaryIndexedTree:
+    def __init__(self, size):
+        self._data = [0] * size
+
+    def add(self, i, x):
+        data = self._data
+        i += 1
+        n = len(data)
+        while i <= n:
+            data[i - 1] += x
+            i += i & -i
+
+    def _sum(self, stop):
+        data = self._data
+        result = 0
+        i = stop
+        while i > 0:
+            result += data[i - 1]
+            i -= i & -i
+        return result
+
+    def range_sum(self, start, stop):
+        return self._sum(stop) - self._sum(start)
+
+
+N, *a = map(int, open(0).read().split())
+
+bit = BinaryIndexedTree(N)
+
+c = 0
+for x in a:
+    c += bit.range_sum(x + 1, N)
+    bit.add(x, 1)
+
+result = [c]
+for i in range(N - 1):
+    c += (N - (a[i] + 1)) - a[i]
+    result.append(c)
+print(*result, sep='\n')
