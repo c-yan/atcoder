f29c27379c6d65f65b90d87d9b6a34d2c96a7124
c-yan <c-yan@users.noreply.github.com>
2021-02-07 00:29:16 +0900

    abc191c

---
+++ abc/abc191/abc191c.py
@@ -0,0 +1,18 @@
+H, W = map(int, input().split())
+S = [input() for _ in range(H)]
+
+result = 0
+for y in range(H):
+    for x in range(W):
+        t = 0
+        if S[y][x] == '#':
+            t += 1
+        if S[y][x + 1] == '#':
+            t += 1
+        if S[y + 1][x] == '#':
+            t += 1
+        if S[y + 1][x + 1] == '#':
+            t += 1
+        if t == 1 or t == 3:
+            result += 1
+print(result)
