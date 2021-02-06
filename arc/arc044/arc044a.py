19348c3621f54b09080669ed626f1be900d50372
c-yan <c-yan@users.noreply.github.com>
2021-02-06 12:17:45 +0900

    arc044a ソースコードの改善

--- arc/arc044/arc044a.py
+++ arc/arc044/arc044a.py
@@ -1,20 +1,25 @@
-N = int(input())
-
-
 def is_prime(n):
-    for i in range(2, int(n ** 0.5) + 1):
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
 
 
 def is_pseudo_prime(n):
+    if n == 1:
+        return False
     return (n % 2 != 0) and (n % 5 != 0) and (n % 3 != 0)
 
 
-if N == 1:
-    print('Not Prime')
-elif is_prime(N) or is_pseudo_prime(N):
+N = int(input())
+
+if is_prime(N) or is_pseudo_prime(N):
     print('Prime')
 else:
     print('Not Prime')
