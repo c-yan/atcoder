37a399ab70272634fb8388a6e1919c9709eb0f2a
c-yan <c-yan@users.noreply.github.com>
2021-02-13 07:01:10 +0900

    abc188d ソースコードの改善

--- abc/abc188/abc188d-2.py
+++ abc/abc188/abc188d-2.py
@@ -3,26 +3,33 @@ from itertools import accumulate
 
 readline = stdin .readline
 
+
+def shrink(a):
+    inv = sorted(set(a))
+    n = len(inv)
+    fwd = {}
+    for i in range(n):
+        fwd[inv[i]] = i
+    return n, fwd, inv
+
+
 N, C = map(int, readline().split())
 abc = [tuple(map(int, readline().split())) for _ in range(N)]
 
-p = set()
+p = []
 for a, b, _ in abc:
-    p.add(a)
-    p.add(b)
-    p.add(b + 1)
-inv = sorted(p)
-fwd = {}
-for i in range(len(inv)):
-    fwd[inv[i]] = i
+    p.append(a)
+    p.append(b)
+    p.append(b + 1)
+n, fwd, inv = shrink(p)
 
-t = [0] * len(inv)
+t = [0] * n
 for a, b, c in abc:
     t[fwd[a]] += c
     t[fwd[b + 1]] -= c
 t = list(accumulate(t))
 
 result = 0
-for i in range(len(t) - 1):
+for i in range(n - 1):
     result += min(t[i], C) * (inv[i + 1] - inv[i])
 print(result)
