7b71b215edce09e3a38976fe25ae29b625de8193
c-yan <c-yan@users.noreply.github.com>
2021-02-13 07:29:20 +0900

    abc113c ソースコードの改善

--- abc/abc113/abc113c.py
+++ abc/abc113/abc113c.py
@@ -1,8 +1,11 @@
 # 二分探索
+from sys import stdin
 from bisect import bisect_left
 
-N, M = map(int, input().split())
-PY = [list(map(int, input().split())) for _ in range(M)]
+readline = stdin.readline
+
+N, M = map(int, readline().split())
+PY = [tuple(map(int, readline().split())) for _ in range(M)]
 
 t = [[] for _ in range(N)]
 for P, Y in PY:
@@ -11,5 +14,7 @@ for P, Y in PY:
 for i in range(N):
     t[i].sort()
 
+result = []
 for P, Y in PY:
-    print("%06d%06d" % (P, bisect_left(t[P - 1], Y) + 1))
+    result.append('%06d%06d' % (P, bisect_left(t[P - 1], Y) + 1))
+print(*result, sep='\n')
