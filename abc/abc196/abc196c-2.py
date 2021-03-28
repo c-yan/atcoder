e8f2451c5dd7fcb0f61d6760de27ad5f1f861d80
c-yan <c-yan@users.noreply.github.com>
2021-03-29 00:47:48 +0900

    abc196c ソースコードの改善

--- abc/abc196/abc196c-2.py
+++ abc/abc196/abc196c-2.py
@@ -1,8 +1,7 @@
 N = int(input())
 
-result = 0
-for i in range(1, 1000000):
-    if int(str(i) * 2) > N:
-        break
-    result += 1
-print(result)
+for i in range(1, 1000001):
+    if int(str(i) * 2) <= N:
+        continue
+    print(i - 1)
+    break
