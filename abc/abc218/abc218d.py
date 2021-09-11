863eed98d5b8615eb6a368fd0933a44f70b84d95
c-yan <c-yan@users.noreply.github.com>
2021-09-12 05:41:27 +0900

    abc218d ソースコードの改善

--- abc/abc218/abc218d.py
+++ abc/abc218/abc218d.py
@@ -1,29 +1,15 @@
 N = int(input())
 xy = [tuple(map(int, input().split())) for _ in range(N)]
 
-a = set()
-b = set()
+d = {}
 for x, y in xy:
-    a.add(x)
-    b.add(y)
-c = sorted(a)
-d = sorted(b)
-e = {}
-f = {}
-for i in range(len(c)):
-    e[c[i]] = i
-for i in range(len(d)):
-    f[d[i]] = i
-
-xy = [(e[x], f[y]) for x, y in xy]
-
-t = [set() for _ in range(len(c))]
-for x, y in xy:
-    t[x].add(y)
+    d.setdefault(x, set())
+    d[x].add(y)
+t = list(d.values())
 
 result = 0
-for i in range(len(c)):
-    for j in range(i + 1, len(c)):
-        g = len(t[i] & t[j])
-        result += g * (g - 1) // 2
+for i in range(len(t)):
+    for j in range(i + 1, len(t)):
+        n = len(t[i] & t[j])
+        result += n * (n - 1) // 2
 print(result)
