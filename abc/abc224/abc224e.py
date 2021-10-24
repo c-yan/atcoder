b7249ab23a00c101b65d2f44173f2f865ef1c79c
c-yan <c-yan@users.noreply.github.com>
2021-10-24 16:00:34 +0900

    abc224e ソースコードの改善

--- abc/abc224/abc224e.py
+++ abc/abc224/abc224e.py
@@ -5,15 +5,8 @@ readline = stdin.readline
 H, W, N = map(int, readline().split())
 rca = [tuple(map(int, readline().split())) for _ in range(N)]
 
-my = [-1 for _ in range(H)]
-mx = [-1 for _ in range(W)]
-for r, c, a in rca:
-    r, c = r - 1, c - 1
-    my[r] = max(my[r], a)
-    mx[c] = max(mx[c], a)
-
-dpy = [0] * H
-dpx = [0] * W
+dpy = [-1] * H
+dpx = [-1] * W
 result = [0] * N
 prev = -1
 qy = []
@@ -27,11 +20,8 @@ for i in sorted(range(N), key=lambda x: rca[x][2], reverse=True):
             dpx[x] = max(dpx[x], t)
         qy.clear()
         qx.clear()
-    t = 0
-    if a < my[r]:
-        t = dpy[r] + 1
-    if a < mx[c]:
-        t = max(dpx[c] + 1, t)
+    t = dpy[r] + 1
+    t = max(dpx[c] + 1, t)
     result[i] = t
     if t != 0:
         qy.append((r, t))
